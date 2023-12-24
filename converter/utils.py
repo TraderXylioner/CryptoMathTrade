2a4b5a28507ed302db15f5a6ede0e32d7d99d6c1
T_X <treider.xylioner@gmail.com>
2023-12-24 21:03:39 +0200

    refactor utils.py

--- converter/utils.py
+++ converter/utils.py
@@ -1,41 +1,43 @@
-from type import Order, OrderBook
+from type import Order, OrderList
+
+
+def check_order(order):
+    if isinstance(order, Order):
+        order = order
+    elif isinstance(order, dict):
+        order = Order.from_dict(order)
+    else:
+        raise TypeError('order must be valid dict or Order')
+    return order
+
+
+def check_orderbook(orderbook):
+    if isinstance(orderbook, OrderList):
+        orderbook = orderbook
+    elif isinstance(orderbook, list) and all(isinstance(item, Order) for item in orderbook):
+        orderbook = OrderList(orderbook)
+    elif isinstance(orderbook, list) and all(isinstance(item, dict) for item in orderbook):
+        orderbook = OrderList(orderbook)
+    else:
+        raise TypeError('element must be a valid OrderBook, list of Orders, or list of order dictionaries')
+    return orderbook
 
 
 def __order_type_check(func):
     def wrapper(order: dict[float, float] | Order, *args, **kwargs) -> Order:
-        if isinstance(order, Order):
-            _order = order
-        elif isinstance(order, dict):
-            _order = Order.from_dict(order)
-        else:
-            raise TypeError('order must be valid dict or Order')
-        return func(_order, *args, **kwargs)
+        return func(check_order(order), *args, **kwargs)
     return wrapper
 
 
 def __orderbook_type_check(func):
-    def wrapper(orderbook: OrderBook | list[Order] | list[dict[float, float]], *args, **kwargs) -> OrderBook:
-        if isinstance(orderbook, OrderBook):
-            _orderbook = orderbook
-        elif isinstance(orderbook, list) and all(isinstance(item, Order) for item in orderbook):
-            _orderbook = OrderBook(orderbook)
-        elif isinstance(orderbook, list) and all(isinstance(item, dict) for item in orderbook):
-            _orderbook = OrderBook(orderbook)
-        else:
-            raise TypeError('element must be a valid OrderBook, list of Orders, or list of order dictionaries')
-
-        return func(_orderbook, *args, **kwargs)
-
+    def wrapper(orderbook: OrderList | list[Order] | list[dict[float, float]], *args, **kwargs) -> OrderList:
+        return func(check_orderbook(orderbook), *args, **kwargs)
     return wrapper
 
 
-def __order_type_check_for_converter(func):
-    def wrapper(order: dict[float, float] | Order, relative_price: float) -> Order:
-        return __order_type_check(func)(order, relative_price)
-    return wrapper
-
-
-def __orderbook_type_check_for_converter(func):
-    def wrapper(orderbook: OrderBook | list[Order] | list[dict[float, float]], relative_price: float) -> OrderBook:
-        return __orderbook_type_check(func)(orderbook, relative_price)
+def __two_orderbook_type_check(func):
+    def wrapper(first_orderbook: OrderList | list[Order] | list[dict[float, float]],
+                second_orderbook: OrderList | list[Order] | list[dict[float, float]],
+                *args, **kwargs) -> OrderList:
+        return func(check_orderbook(first_orderbook), check_orderbook(second_orderbook), *args, **kwargs)
     return wrapper
