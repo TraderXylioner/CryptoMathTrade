49cd25278591d665beec58e0ab2321ecb8cdf5cc
T_X <treider.xylioner@gmail.com>
2023-12-24 14:55:27 +0200

    complete converter

--- converter/price.py
+++ converter/price.py
@@ -1,23 +1,18 @@
+from converter.utils import __order_type_check, __orderbook_type_check
 from type import Order, OrderBook
 
 
-# def __check_converted(func):
-#     def wrapper(order: Order, *args, **kwargs):
-#         if order.order_type == 'converted':
-#             raise ValueError('order already converted')
-#         elif order.order_type == 'native':
-#             return func(order, *args, **kwargs)
-#         else:
-#             raise ValueError('order undefined')
-#     return wrapper
+@__order_type_check
+def convert_price_from_order(order: dict[float, float] | Order, relative_price: float) -> Order:
+    """
+    Function converting order relative to relative_price.
+    For example, if the order is on pair BTC/ETH = 18 (for 18 ETH you can buy 1 BTC),
+    ETH/USDT = 2000, and you want to convert in USDT.
+    18 x 2000 = 36000 price BTC in USDT.
+    """
+    return Order(price=order.price * relative_price, volume=order.volume)
 
 
-# @__check_converted
-def convert_price_from_order(order: dict, price: float) -> Order:
-    _order = Order.from_dict(order)
-    return Order(price=price, volume=_order.volume)
-
-
-# @__check_converted
-def convert_price_from_orderbook(orderbook: OrderBook, price: float):
-    ...
+@__orderbook_type_check
+def convert_price_from_orderbook(orderbook: OrderBook | list[Order] | list[dict[float, float]], relative_price: float):
+    return OrderBook([convert_price_from_order(order, relative_price) for order in orderbook.orders])
