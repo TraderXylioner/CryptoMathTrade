6d4d31f193ee9e7267cc671761441bf64a445274
T_X <treider.xylioner@gmail.com>
2023-12-24 15:50:39 +0200

    refactor utils, add global type check

--- converter/price.py
+++ converter/price.py
@@ -1,18 +1,18 @@
-from converter.utils import __order_type_check, __orderbook_type_check
+from converter.utils import __order_type_check_for_converter, __orderbook_type_check_for_converter
 from type import Order, OrderBook
 
 
-@__order_type_check
-def convert_price_from_order(order: dict[float, float] | Order, relative_price: float) -> Order:
+@__order_type_check_for_converter
+def convert_price_from_order(order: Order, relative_price: float) -> Order:
     """
     Function converting order relative to relative_price.
     For example, if the order is on pair BTC/ETH = 18 (for 18 ETH you can buy 1 BTC),
     ETH/USDT = 2000, and you want to convert in USDT.
     18 x 2000 = 36000 price BTC in USDT.
     """
-    return Order(price=order.price * relative_price, volume=order.volume)
+    return Order(price=order.price * relative_price, volume=order.volume)  # maybe не создавать новый объект, а менять старый?
 
 
-@__orderbook_type_check
-def convert_price_from_orderbook(orderbook: OrderBook | list[Order] | list[dict[float, float]], relative_price: float):
-    return OrderBook([convert_price_from_order(order, relative_price) for order in orderbook.orders])
+@__orderbook_type_check_for_converter
+def convert_price_from_orderbook(orderbook: OrderBook, relative_price: float) -> OrderBook:
+    return OrderBook([convert_price_from_order(order, relative_price) for order in orderbook.orders])   # maybe не создавать новый объект, а менять старый?
--- converter/utils.py
+++ converter/utils.py
@@ -2,19 +2,19 @@ from type import Order, OrderBook
 
 
 def __order_type_check(func):
-    def wrapper(order: dict[float, float] | Order, relative_price: float) -> Order:
+    def wrapper(order: dict[float, float] | Order, *args, **kwargs) -> Order:
         if isinstance(order, Order):
             _order = order
         elif isinstance(order, dict):
             _order = Order.from_dict(order)
         else:
             raise TypeError('order must be valid dict or Order')
-        return func(_order, relative_price)
+        return func(_order, *args, **kwargs)
     return wrapper
 
 
 def __orderbook_type_check(func):
-    def wrapper(orderbook: OrderBook | list[Order] | list[dict[float, float]], relative_price: float) -> Order:
+    def wrapper(orderbook: OrderBook | list[Order] | list[dict[float, float]], *args, **kwargs) -> OrderBook:
         if isinstance(orderbook, OrderBook):
             _orderbook = orderbook
         elif isinstance(orderbook, list) and all(isinstance(item, Order) for item in orderbook):
@@ -24,6 +24,18 @@ def __orderbook_type_check(func):
         else:
             raise TypeError('element must be a valid OrderBook, list of Orders, or list of order dictionaries')
 
-        return func(_orderbook, relative_price)
+        return func(_orderbook, *args, **kwargs)
 
+    return wrapper
+
+
+def __order_type_check_for_converter(func):
+    def wrapper(order: dict[float, float] | Order, relative_price: float) -> Order:
+        return __order_type_check(func)(order, relative_price)
+    return wrapper
+
+
+def __orderbook_type_check_for_converter(func):
+    def wrapper(orderbook: OrderBook | list[Order] | list[dict[float, float]], relative_price: float) -> OrderBook:
+        return __orderbook_type_check(func)(orderbook, relative_price)
     return wrapper
