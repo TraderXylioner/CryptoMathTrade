3b773d4fec21388ac16d0be1717bce37ed8d6eeb
T_X <treider.xylioner@gmail.com>
2023-12-24 21:04:03 +0200

    refactor price.py

--- converter/price.py
+++ converter/price.py
@@ -1,5 +1,17 @@
-from converter.utils import __order_type_check_for_converter, __orderbook_type_check_for_converter
-from type import Order, OrderBook
+from converter.utils import __order_type_check, __orderbook_type_check
+from type import Order, OrderList
+
+
+def __order_type_check_for_converter(func):
+    def wrapper(order: dict[float, float] | Order, relative_price: float) -> Order:
+        return __order_type_check(func)(order, relative_price)
+    return wrapper
+
+
+def __orderbook_type_check_for_converter(func):
+    def wrapper(orderbook: OrderList | list[Order] | list[dict[float, float]], relative_price: float) -> OrderList:
+        return __orderbook_type_check(func)(orderbook, relative_price)
+    return wrapper
 
 
 @__order_type_check_for_converter
@@ -14,5 +26,5 @@ def convert_price_from_order(order: Order, relative_price: float) -> Order:
 
 
 @__orderbook_type_check_for_converter
-def convert_price_from_orderbook(orderbook: OrderBook, relative_price: float) -> OrderBook:
-    return OrderBook([convert_price_from_order(order, relative_price) for order in orderbook.orders])   # maybe не создавать новый объект, а менять старый?
+def convert_price_from_orderbook(orderbook: OrderList, relative_price: float) -> OrderList:
+    return OrderList([convert_price_from_order(order, relative_price) for order in orderbook.orders])   # maybe не создавать новый объект, а менять старый?
