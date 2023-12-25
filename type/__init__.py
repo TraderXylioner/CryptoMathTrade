8c715b5f4c5c11db271b956f8372002d545c3553
T_X <treider.xylioner@gmail.com>
2023-12-25 12:57:19 +0200

    add deal.py

--- type/__init__.py
+++ type/__init__.py
@@ -1,1 +1,2 @@
 from type.order import Order, OrderList, OrderBook
+from type.deal import ArbitrageDeal, ArbitrageDeals
---
+++ type/deal.py
@@ -0,0 +1,42 @@
+from decimal import Decimal
+
+
+class ArbitrageDeal:
+    def __init__(self, price_buy: float, price_sell: float, volume: float, spread: float | None = None):
+        self.price_buy: Decimal = Decimal(price_buy)
+        self.price_sell: Decimal = Decimal(price_sell)
+        self.volume: Decimal = Decimal(volume)
+        self.spread: Decimal = Decimal(spread) if spread else None
+
+    def __repr__(self):
+        return '{' + f"'price_buy': {float(self.price_buy)}, " \
+               f"price_sell: {float(self.price_sell)}, " \
+               f"'volume': {float(self.volume)}, " \
+               f"'spread': {float(self.spread)}" \
+               + '}'
+
+
+class ArbitrageDeals:
+    def __init__(self, deals: list[ArbitrageDeal] = None):
+        self.deals = deals if deals else []
+
+    def __repr__(self):
+        return f'{self.__dict__}'
+
+    def __getitem__(self, index):
+        return self.deals[index]
+
+    def __setitem__(self, index, value):
+        self.deals[index] = value
+
+    def __delitem__(self, index):
+        del self.deals[index]
+
+    def pop(self, index=None):
+        if index is None:
+            index = -1
+        item = self.deals.pop(index)
+        return item
+
+    def append(self, deal):
+        self.deals.append(deal)
