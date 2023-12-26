1e08f679d22134132194c277badd68d655586dcb
T_X <treider.xylioner@gmail.com>
2023-12-26 13:18:54 +0200

    rename main object

--- type/__init__.py
+++ type/__init__.py
@@ -1,2 +1,3 @@
 from type.order import Order, OrderList, OrderBook
-from type.deal import ArbitrageDeal, ArbitrageDeals
+from type.deal import Deals, Deal
+from type.arbitrage import ArbitrageDeal, ArbitrageDeals
