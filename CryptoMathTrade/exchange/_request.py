99ad094bf17250523aaec0d8289115b9049e88a6
T_X <treider.xylioner@gmail.com>
2024-01-18 22:50:58 +0200

    add async sender

--- CryptoMathTrade/exchange/_request.py
+++ CryptoMathTrade/exchange/_request.py
@@ -23,24 +23,21 @@ class Request:
         response = _dispatch_request(self.session, method)(**params)
         return response
 
-    def _dispatch_request(self, http_method):
-        return {
-            'GET': self.session.get,
-            'DELETE': self.session.delete,
-            'PUT': self.session.put,
-            'POST': self.session.post,
-        }.get(http_method, 'GET')
 
+class AsyncRequest:
+    def __init__(self, timeout=None, proxies=None, headers=None):
+        self.timeout = timeout
+        self.proxies = proxies
+        self.headers = headers
 
-# class AsyncRequest:
-#     @classmethod
-#     async def send_async_request(cls,
-#                                  url: str,
-#                                  headers: dict | None = None,
-#                                  proxy: str | None = None
-#                                  ):
-#         async with aiohttp.ClientSession(headers=headers) as session:
-#             async with session.get(url=url, proxy=proxy, ssl=True) as response:
-#                 print(response)
-#                 response = await response.json()
-#                 print(response)
+    async def send_request(self, method: str, url: str, payload: dict | None = None):
+        if payload is None:
+            payload = {}
+        params = clean_none_value({'url': url,
+                                   'params': _prepare_params(payload),
+                                   'timeout': self.timeout,
+                                   'proxies': self.proxies,
+                                   })
+        async with aiohttp.ClientSession() as session:
+            async with _dispatch_request(session, method)(**params) as response:
+                return await response.json()
