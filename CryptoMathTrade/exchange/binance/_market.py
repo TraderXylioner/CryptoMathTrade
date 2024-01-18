0801cac9c4a721f6c84b852c28ca2076471ba79a
T_X <treider.xylioner@gmail.com>
2024-01-18 22:51:15 +0200

    refactor market

--- CryptoMathTrade/exchange/binance/_market.py
+++ CryptoMathTrade/exchange/binance/_market.py
@@ -1,31 +1,45 @@
 from .api import API
-from .setting import DEPTH_URL, TRADES_URL, PRICE_URL, TICKER_URL
-from ..utils import convert_list_to_json_array
+from .core import get_depth_args, get_trades_args, get_ticker_args, get_price_args
 
 
 class Market(API):
-    def get_depth(self, symbol: str, limit: int | None = None):
-        params = {'symbol': symbol, 'limit': limit}
-        return self._query(DEPTH_URL, params)
+    def get_depth(self,
+                  symbol: str,
+                  limit: int | None = None
+                  ):
+        return self._query(**get_depth_args(symbol=symbol, limit=limit))
 
     def get_trades(self, symbol: str, limit: int | None = None):
-        params = {'symbol': symbol, 'limit': limit}
-        return self._query(TRADES_URL, params)
+        return self._query(**get_trades_args(symbol=symbol, limit=limit))
 
     def get_price(self,
                   symbol: str | None = None,
                   symbols: list | None = None):
-        params = {'symbol': symbol, 'symbols': convert_list_to_json_array(symbols)}
-        return self._query(PRICE_URL, params)
+        return self._query(**get_price_args(symbol=symbol, symbols=symbols))
 
     def get_ticker(self,
                    symbol: str | None = None,
                    symbols: list | None = None):
-        if symbol and symbols:
-            raise ValueError('symbol and symbols cannot be sent together.')
-        params = {'symbol': symbol, 'symbols': convert_list_to_json_array(symbols)}
-        return self._query(TICKER_URL, params)
+        return self._query(**get_ticker_args(symbol=symbol, symbols=symbols))
 
 
-class AsyncMarket:
-    ...
+class AsyncMarket(API):
+    async def get_depth(self,
+                        symbol: str,
+                        limit: int | None = None):
+        return await self._async_query(**get_depth_args(symbol=symbol, limit=limit))
+
+    async def get_trades(self,
+                         symbol: str,
+                         limit: int | None = None):
+        return self._async_query(**get_trades_args(symbol=symbol, limit=limit))
+
+    async def get_price(self,
+                        symbol: str | None = None,
+                        symbols: list | None = None):
+        return self._async_query(**get_price_args(symbol=symbol, symbols=symbols))
+
+    async def get_ticker(self,
+                         symbol: str | None = None,
+                         symbols: list | None = None):
+        return self._async_query(**get_ticker_args(symbol=symbol, symbols=symbols))
