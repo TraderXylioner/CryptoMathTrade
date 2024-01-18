2887fae94a44c751343a505863f2a0d4aedce311
T_X <treider.xylioner@gmail.com>
2024-01-18 22:48:56 +0200

    add TimeInForce and Side type

--- CryptoMathTrade/type/order.py
+++ CryptoMathTrade/type/order.py
@@ -95,3 +95,21 @@ class OrderBook:
 
     def __repr__(self):
         return f'asks: {self.asks}\nbids: {self.bids}'
+
+
+class TimeInForce:
+    """
+    GTC	Good Til Canceled An order will be on the book unless the order is canceled.
+
+    IOC	Immediate Or Cancel An order will try to fill the order as much as it can before the order expires.
+
+    FOK	Fill or Kill An order will expire if the full order cannot be filled upon execution.
+    """
+    GTC = 'GTC'
+    IOC = 'IOC'
+    FOK = 'FOK'
+
+
+class Side:
+    BUY = 'BUY'
+    SELL = 'SELL'
