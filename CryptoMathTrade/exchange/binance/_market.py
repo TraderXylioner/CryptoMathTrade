c621f1a4883ebfa1df72018029caa8fe73af1595
T_X <treider.xylioner@gmail.com>
2024-01-19 14:30:57 +0200

    add typing

--- CryptoMathTrade/exchange/binance/_market.py
+++ CryptoMathTrade/exchange/binance/_market.py
@@ -8,39 +8,47 @@ class Market(API):
                   symbol: str,
                   limit: int | None = None
                   ):
-        return self._query(**get_depth_args(symbol=symbol, limit=limit))
+        res = self._query(**get_depth_args(symbol=symbol, limit=limit))
+        return OrderBook.from_list(res.json())
 
     def get_trades(self, symbol: str, limit: int | None = None):
-        return self._query(**get_trades_args(symbol=symbol, limit=limit))
+        res = self._query(**get_trades_args(symbol=symbol, limit=limit))
+        return Trades([Trade(**trade) for trade in res.json()])
 
     def get_price(self,
                   symbol: str | None = None,
                   symbols: list | None = None):
-        return self._query(**get_price_args(symbol=symbol, symbols=symbols))
+        res = self._query(**get_price_args(symbol=symbol, symbols=symbols))
+        return Price(**res.json())
 
     def get_ticker(self,
                    symbol: str | None = None,
                    symbols: list | None = None):
-        return self._query(**get_ticker_args(symbol=symbol, symbols=symbols))
+        res = self._query(**get_ticker_args(symbol=symbol, symbols=symbols))
+        return Ticker(**res.json())
 
 
 class AsyncMarket(API):
     async def get_depth(self,
                         symbol: str,
                         limit: int | None = None):
-        return await self._async_query(**get_depth_args(symbol=symbol, limit=limit))
+        res = await self._async_query(**get_depth_args(symbol=symbol, limit=limit))
+        return OrderBook.from_list(res)
 
     async def get_trades(self,
                          symbol: str,
                          limit: int | None = None):
-        return self._async_query(**get_trades_args(symbol=symbol, limit=limit))
+        res = await self._async_query(**get_trades_args(symbol=symbol, limit=limit))
+        return Trades([Trade(**trade) for trade in res])
 
     async def get_price(self,
                         symbol: str | None = None,
                         symbols: list | None = None):
-        return self._async_query(**get_price_args(symbol=symbol, symbols=symbols))
+        res = await self._async_query(**get_price_args(symbol=symbol, symbols=symbols))
+        return Price(**res)
 
     async def get_ticker(self,
                          symbol: str | None = None,
                          symbols: list | None = None):
-        return self._async_query(**get_ticker_args(symbol=symbol, symbols=symbols))
+        res = await self._async_query(**get_ticker_args(symbol=symbol, symbols=symbols))
+        return Ticker(**res)
