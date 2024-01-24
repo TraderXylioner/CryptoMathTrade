ec17dbb6464d8e72a11875e454e10229d63cb952
T_X <treider.xylioner@gmail.com>
2024-01-24 17:19:22 +0200

    refactor ticker.py

--- CryptoMathTrade/type/ticker.py
+++ CryptoMathTrade/type/ticker.py
@@ -6,15 +6,15 @@ class Ticker:
     symbol: str
     priceChange: float
     priceChangePercent: float
-    weightedAvgPrice: float
     openPrice: float
     highPrice: float
     lowPrice: float
     lastPrice: float
     volume: float
     quoteVolume: float
-    openTime: int
     closeTime: int
-    firstId: int
-    lastId: int
-    count: int
+    openTime: int | None = None
+    weightedAvgPrice: float | None = None
+    firstId: int | None = None
+    lastId: int | None = None
+    count: int | None = None
