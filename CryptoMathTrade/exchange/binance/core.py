edc77efbdce44a4d29349682fdfadb33727d2eaf
T_X <treider.xylioner@gmail.com>
2024-01-24 17:19:39 +0200

    add type

--- CryptoMathTrade/exchange/binance/core.py
+++ CryptoMathTrade/exchange/binance/core.py
@@ -19,22 +19,22 @@ def ws_get_trades_args(params: dict) -> dict:
 
 # Market
 @convert_kwargs_to_dict
-def get_depth_args(params):
+def get_depth_args(params: dict) -> dict:
     return {'method': 'GET', 'url': BASE_URL + DEPTH_URL, 'params': params}
 
 
 @convert_kwargs_to_dict
-def get_trades_args(params):
+def get_trades_args(params: dict) -> dict:
     return {'method': 'GET', 'url': BASE_URL + TRADES_URL, 'params': params}
 
 
 @convert_kwargs_to_dict
-def get_price_args(params):
+def get_price_args(params: dict) -> dict:
     return {'method': 'GET', 'url': BASE_URL + PRICE_URL, 'params': params}
 
 
 @convert_kwargs_to_dict
-def get_ticker_args(params):
+def get_ticker_args(params: dict) -> dict:
     if params.get('symbol') and params.get('symbols'):
         raise ValueError('symbol and symbols cannot be sent together.')
     return {'method': 'GET', 'url': BASE_URL + TICKER_URL, 'params': params}
@@ -42,33 +42,33 @@ def get_ticker_args(params):
 
 # Spot
 @convert_kwargs_to_dict
-def get_orders_args(SpotObj, params):
+def get_orders_args(SpotObj, params: dict) -> dict:
     return {'method': 'GET', 'url': BASE_URL + GET_ORDERS_URL, 'params': SpotObj.get_payload(params)}
 
 
 @convert_kwargs_to_dict
-def get_open_order_args(SpotObj, params):
+def get_open_order_args(SpotObj, params: dict) -> dict:
     if not params.get('orderId') and not params.get('origClientOrderId'):
         raise ValueError('Param "origClientOrderId" or "orderId" must be sent, but both were empty/null!')
     return {'method': 'GET', 'url': BASE_URL + ORDER_URL, 'params': SpotObj.get_payload(params)}
 
 
 @convert_kwargs_to_dict
-def get_open_orders_args(SpotObj, params):
+def get_open_orders_args(SpotObj, params: dict) -> dict:
     return {'method': 'GET', 'url': BASE_URL + OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
 
 
 @convert_kwargs_to_dict
-def delete_open_orders_args(SpotObj, params):
+def delete_open_orders_args(SpotObj, params: dict) -> dict:
     return {'method': 'DELETE', 'url': BASE_URL + OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
 
 
 @convert_kwargs_to_dict
-def new_order_args(SpotObj, params):
+def new_order_args(SpotObj, params: dict) -> dict:
     return {'method': 'POST', 'url': BASE_URL + ORDER_URL, 'params': SpotObj.get_payload(params)}
 
 
 # Account
 @convert_kwargs_to_dict
-def get_deposit_address_args(AccountObj, params):
+def get_deposit_address_args(AccountObj, params: dict) -> dict:
     return {'method': 'GET', 'url': BASE_URL + GET_DEPOSIT_ADDRESS, 'params': AccountObj.get_payload(params)}
