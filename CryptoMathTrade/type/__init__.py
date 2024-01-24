b92d8515ee365bf66e81afb68d492dd447cb6ce3
T_X <treider.xylioner@gmail.com>
2024-01-24 20:13:33 +0200

    remove price.py

--- CryptoMathTrade/type/__init__.py
+++ CryptoMathTrade/type/__init__.py
@@ -3,4 +3,3 @@ from CryptoMathTrade.type.deal import Deals, Deal
 from CryptoMathTrade.type.arbitrage import ArbitrageDeal, ArbitrageDeals
 from CryptoMathTrade.type.trade import Trade
 from CryptoMathTrade.type.ticker import Ticker
-from CryptoMathTrade.type.price import Price
--- CryptoMathTrade/type/price.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Price:
-    symbol: str
-    price: float
