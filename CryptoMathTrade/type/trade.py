0688b8bbeae00e227f5204f63315820b4e6a3f77
T_X <treider.xylioner@gmail.com>
2024-01-24 17:19:10 +0200

    refactor trade.py

--- CryptoMathTrade/type/trade.py
+++ CryptoMathTrade/type/trade.py
@@ -6,12 +6,7 @@ class Trade:
     id: int
     price: float
     qty: float
-    quoteQty: float
     time: int
-    isBuyerMaker: bool
-    isBestMatch: bool
-
-
-@dataclass
-class Trades:
-    trades: list
+    isBuyerMaker: bool | None = None
+    quoteQty: float | None = None
+    isBestMatch: bool | None = None
