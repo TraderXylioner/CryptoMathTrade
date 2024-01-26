e005b8303f991ce34a497330fb687c75aa44748e
T_X <treider.xylioner@gmail.com>
2024-01-26 13:21:02 +0200

    add docs

--- CryptoMathTrade/exchange/bingx/core.py
+++ CryptoMathTrade/exchange/bingx/core.py
@@ -5,27 +5,80 @@ from ..utils import _convert_kwargs_to_dict, get_timestamp
 # WS Market
 @_convert_kwargs_to_dict
 def ws_get_depth_args(params: dict) -> dict:
+    """Partial Book Depth Streams
+
+    Top bids and asks.
+
+    Stream Names: <symbol>@depth<levels>.
+
+    param:
+        symbol (str): the trading pair
+    """
     return {'method': 'sub', 'url': URLS.WS_BASE_URL, 'params': f'{params["symbol"]}@depth'}
 
 
 @_convert_kwargs_to_dict
 def ws_get_trades_args(params: dict) -> dict:
+    """Trade Streams
+
+     The Trade Streams push raw trade information; each trade has a unique buyer and seller.
+
+     Stream Name: <symbol>@trade
+
+     param:
+        symbol (str): the trading pair
+
+     Update Speed: Real-time
+     """
     return {'method': 'sub', 'url': URLS.WS_BASE_URL, 'params': f'{params["symbol"]}@trade'}
 
 
 # Market
 @_convert_kwargs_to_dict
 def get_depth_args(params: dict) -> dict:
+    """Get orderbook.
+
+    GET /openApi/spot/v1/market/depth
+
+    https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#Query%20depth%20information
+
+    param:
+        symbol (str): the trading pair
+        limit (int, optional): limit the results. Default 100; max 1000. If limit > 1000, then the response will truncate to 1000
+        recvWindow (int, optional).
+    """
+    params['limit'] = min(int(params.get('limit', 100)), 1000)
     return {'method': 'GET', 'url': URLS.BASE_URL + URLS.DEPTH_URL, 'params': params}
 
 
 @_convert_kwargs_to_dict
 def get_trades_args(params: dict) -> dict:
+    """Recent Trades List
+    Get recent trades (up to last 100).
+
+    GET /openApi/spot/v1/market/trades
+
+    https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#Query%20transaction%20records
+
+    params:
+        symbol (str): the trading pair
+        limit (int, optional): limit the results. Default 100; max 100
+        recvWindow (int, optional).
+    """
     return {'method': 'GET', 'url': URLS.BASE_URL + URLS.TRADES_URL, 'params': params}
 
 
 @_convert_kwargs_to_dict
 def get_ticker_args(params: dict) -> dict:
+    """24hr Ticker Price Change Statistics
+
+    GET /openApi/spot/v1/ticker/24hr
+
+    https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#24-hour%20price%20changes
+
+    params:
+        symbol (str, optional): the trading pair.
+    """
     params['timestamp'] = get_timestamp()
     return {'method': 'GET', 'url': URLS.BASE_URL + URLS.TICKER_URL, 'params': params}
 
