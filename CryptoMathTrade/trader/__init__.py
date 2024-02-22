30143b84f884c268f76cb0ce2041030ac762fba8
T_X <treider.xylioner@gmail.com>
2024-02-22 20:28:56 +0200

    import

--- CryptoMathTrade/trader/__init__.py
+++ CryptoMathTrade/trader/__init__.py
@@ -1,4 +1,3 @@
-class Trader:
-    from CryptoMathTrade.trader.price import convert_price_in_order, convert_price_in_orderbook
-    from CryptoMathTrade.trader.trade import calculate_spread, trade_by_volume, trade_by_amount
-    from CryptoMathTrade.trader.calculate import get_spread
+from .trade import trade_by_volume, trade_by_amount
+from .calculate import get_spread, convert_price_in_order, convert_price_in_order_list
+from .utils import check_fee
