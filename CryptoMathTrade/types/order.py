b0aad11877e90a4f7f3ef6192fa91ef8f8208308
T_X <treider.xylioner@gmail.com>
2024-02-22 22:20:36 +0200

    add OrderBook, TimeInForce types

--- CryptoMathTrade/types/order.py
+++ CryptoMathTrade/types/order.py
@@ -1,4 +1,6 @@
 from decimal import Decimal
+from enum import Enum
+
 from pydantic import BaseModel
 
 
@@ -24,3 +26,21 @@ class OrderBook(BaseModel):
     """
     asks: list[Order]
     bids: list[Order]
+
+
+class TimeInForce(Enum):
+    """
+    GTC	Good Til Canceled An order will be on the book unless the order is canceled.
+
+    IOC	Immediate Or Cancel An order will try to fill the order as much as it can before the order expires.
+
+    FOK	Fill or Kill An order will expire if the full order cannot be filled upon execution.
+    """
+    GTC = 'GTC'
+    IOC = 'IOC'
+    FOK = 'FOK'
+
+
+class Side(Enum):
+    BUY = 'BUY'
+    SELL = 'SELL'
