a4e3a3092d883b5836dc1f7bfc15672ce1cd139c
T_X <treider.xylioner@gmail.com>
2024-02-22 19:07:52 +0200

    refactor calculate

--- CryptoMathTrade/trader/calculate.py
+++ CryptoMathTrade/trader/calculate.py
@@ -1,5 +1,19 @@
 from decimal import Decimal
 
 
-def get_spread(ask: float | Decimal, bid: float | Decimal) -> Decimal:
-    return ((Decimal(bid) - Decimal(ask)) / Decimal(bid)) * Decimal(100)
+def get_spread(ask: Decimal | float,
+               bid: Decimal | float,
+               ) -> Decimal:
+    """
+    Calculate the spread between bid and ask prices.
+
+    Args:
+        ask (Decimal | float): The ask price.
+        bid (Decimal | float): The bid price.
+
+    Returns:
+        Decimal: The spread expressed as a decimal representation of a percentage.
+                It represents the difference between the bid and ask prices,
+                divided by the bid price.
+    """
+    return (Decimal(bid) - Decimal(ask)) / Decimal(bid)
