76f951a0f974e3bab47e28c6395cb71812a1526e
T_X <treider.xylioner@gmail.com>
2024-02-22 20:51:51 +0200

    move get_spread to utils

--- CryptoMathTrade/trader/calculate.py
+++ CryptoMathTrade/trader/calculate.py
@@ -1,24 +1,7 @@
 from decimal import Decimal
 
-from CryptoMathTrade.types import Order
-
-
-def get_spread(ask: Decimal | float,
-               bid: Decimal | float,
-               ) -> Decimal:
-    """
-    Calculate the spread between bid and ask prices.
-
-    Args:
-        ask (Decimal | float): The ask price.
-        bid (Decimal | float): The bid price.
-
-    Returns:
-        Decimal: The spread expressed as a decimal representation of a percentage.
-                It represents the difference between the bid and ask prices,
-                divided by the bid price.
-    """
-    return (Decimal(bid) - Decimal(ask)) / Decimal(bid)
+from .utils import check_fee, get_spread
+from CryptoMathTrade.types import Order, ArbitrageDeal
 
 
 def convert_price_in_order(order: Order, relative_price: float | Decimal) -> Order:
