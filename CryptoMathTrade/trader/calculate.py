f30a5a79fa102e3b1e6b8ae5e7ceee5c15cbaba8
T_X <treider.xylioner@gmail.com>
2024-02-22 20:04:09 +0200

    refactor convert_price_in_order and convert_price_in_order_list

--- CryptoMathTrade/trader/calculate.py
+++ CryptoMathTrade/trader/calculate.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 
+from CryptoMathTrade.types import Order
+
 
 def get_spread(ask: Decimal | float,
                bid: Decimal | float,
@@ -17,3 +19,41 @@ def get_spread(ask: Decimal | float,
                 divided by the bid price.
     """
     return (Decimal(bid) - Decimal(ask)) / Decimal(bid)
+
+
+def convert_price_in_order(order: Order, relative_price: float | Decimal) -> Order:
+    """
+    Converts the price of an order by applying a relative price factor.
+
+    For example, if the order is on pair BTC/ETH = 18 (for 18 ETH you can buy 1 BTC),
+    ETH/USDT = 2000, and you want to convert in USDT.
+    18 x 2000 = 36000 price BTC in USDT.
+
+    Args:
+        order (Order): The order to be modified.
+        relative_price (float | Decimal): The relative price factor to apply.
+            It can be specified as a float or a Decimal.
+
+    Returns:
+        Order: The modified order with the price converted.
+    """
+    return Order(price=order.price * Decimal(relative_price), volume=order.volume)
+
+
+def convert_price_in_order_list(order_list: list[Order], relative_price: float | Decimal) -> list[Order]:
+    """
+    Converts the prices of a list of orders by applying a relative price factor.
+
+    For example, if the order is on pair BTC/ETH = 18 (for 18 ETH you can buy 1 BTC),
+    ETH/USDT = 2000, and you want to convert in USDT.
+    18 x 2000 = 36000 price BTC in USDT.
+
+    Args:
+        order_list (list[Order]): The list of orders to be modified.
+        relative_price (float | Decimal): The relative price factor to apply.
+            It can be specified as a float or a Decimal.
+
+    Returns:
+        list[Order]: The list of modified orders with the prices converted.
+    """
+    return [convert_price_in_order(order, relative_price) for order in order_list]
