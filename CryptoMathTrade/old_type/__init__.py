1f5d555d4e8bf5b2822e21587e76e79435901fc3
T_X <treider.xylioner@gmail.com>
2024-02-22 21:45:36 +0200

    remove old types

--- CryptoMathTrade/old_type/__init__.py
+++
@@ -1,5 +0,0 @@
-from CryptoMathTrade.type.order import Order, OrderList, OrderBook, Side, TimeInForce
-from CryptoMathTrade.type.deal import Deals, Deal
-from CryptoMathTrade.type.arbitrage import ArbitrageDeal, ArbitrageDeals
-from CryptoMathTrade.type.trade import Trade
-from CryptoMathTrade.type.ticker import Ticker
