7dad9d6e523d320756f316f579848ae8169da051
T_X <treider.xylioner@gmail.com>
2024-02-23 10:09:05 +0200

    add exchanges to ExchangeCores

--- CryptoMathTrade/exchange/__init__.py
+++ CryptoMathTrade/exchange/__init__.py
@@ -1,6 +1,12 @@
 from .binance.core import Core as binance_core
 from .kucoin.core import Core as kucoin_core
 from .okx.core import Core as okx_core
+from .htx.core import Core as htx_core
+from .bitmart.core import Core as bitmart_core
+from .gate.core import Core as gate_core
+from .mexc.core import Core as mexc_core
+from .bitget.core import Core as bitget_core
+from .ascendex.core import Core as ascendex_core
 
 
 class ExchangeCores:
@@ -11,6 +17,12 @@ class ExchangeCores:
         'binance': binance_core,
         'kucoin': kucoin_core,
         'okx': okx_core,
+        'htx': htx_core,
+        'bitmart': bitmart_core,
+        'gate': gate_core,
+        'mexc': mexc_core,
+        'bitget': bitget_core,
+        'ascendex': ascendex_core,
     }
 
     @classmethod
