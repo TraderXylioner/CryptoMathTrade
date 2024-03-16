d78b600a01560ae27e9c059632be25dbd609380f
T_X <treider.xylioner@gmail.com>
2024-03-16 21:42:17 +0200

    refactor trade

--- CryptoMathTrade/types/trade.py
+++ CryptoMathTrade/types/trade.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 class Trade(BaseModel):
     id: int
     price: float
-    qty: float
+    quantity: float
     time: int
-    isBuyerMaker: bool | None = None
-    quoteQty: float | None = None
-    isBestMatch: bool | None = None
+    # quoteQuantity: float = price * quantity
+    # isBuyerMaker: bool | None = None
+    # isBestMatch: bool | None = None
