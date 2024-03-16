4172aee7435ae304439613aa45fa5e2aa3cf1429
T_X <treider.xylioner@gmail.com>
2024-03-16 21:42:52 +0200

    import validate_response

--- CryptoMathTrade/exchange/binance/_market.py
+++ CryptoMathTrade/exchange/binance/_market.py
@@ -3,6 +3,7 @@ from typing import Generator
 from ._api import API
 from .core import MarketCore, WSMarketCore
 from CryptoMathTrade.types import OrderBook, Trade, Ticker, Order
+from ..utils import validate_response
 
 
 class Market(API):
