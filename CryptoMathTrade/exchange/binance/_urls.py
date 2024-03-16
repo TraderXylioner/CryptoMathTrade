a4bc239429974951031a27aee46a30ba00992056
T_X <treider.xylioner@gmail.com>
2024-03-16 04:25:10 +0200

    add coms

--- CryptoMathTrade/exchange/binance/_urls.py
+++ CryptoMathTrade/exchange/binance/_urls.py
@@ -1,13 +1,15 @@
 class URLS:
     # HTTP
     BASE_URL = 'https://api.binance.com'
-    DEPTH_URL = '/api/v3/depth'
-    TRADES_URL = '/api/v3/trades'
-    TICKER_URL = '/api/v3/ticker'
+    DEPTH_URL = '/api/v3/depth'  # https://binance-docs.github.io/apidocs/spot/en/#order-book
+    TRADES_URL = '/api/v3/trades'  # https://binance-docs.github.io/apidocs/spot/en/#recent-trades-list
+    TICKER_URL = '/api/v3/ticker'  # https://binance-docs.github.io/apidocs/spot/en/#24hr-ticker-price-change-statistics
 
-    ORDER_URL = '/api/v3/order'
-    GET_ORDERS_URL = '/api/v3/allOrders'
-    OPEN_ORDERS_URL = '/api/v3/openOrders'
+    ORDER_URL = '/api/v3/order'  # https://binance-docs.github.io/apidocs/spot/en/#query-order-user_data or
+    # https://binance-docs.github.io/apidocs/spot/en/#cancel-order-trade or
+    # https://binance-docs.github.io/apidocs/spot/en/#new-order-trade
+    GET_ORDERS_URL = '/api/v3/allOrders'  # https://binance-docs.github.io/apidocs/spot/en/#all-orders-user_data
+    OPEN_ORDERS_URL = '/api/v3/openOrders'  # https://binance-docs.github.io/apidocs/spot/en/#current-open-orders-user_data
 
     GET_DEPOSIT_ADDRESS = '/sapi/v1/capital/deposit/address'
 
