6378d2c1ab98d163da3396044d38d64a5f99863c
T_X <treider.xylioner@gmail.com>
2024-03-16 04:25:36 +0200

    refactor core

--- CryptoMathTrade/exchange/binance/_api.py
+++ CryptoMathTrade/exchange/binance/_api.py
@@ -6,24 +6,71 @@ from ..utils import get_timestamp, hmac_hashing, _prepare_params, check_api_keys
 
 
 class API:
-    def __init__(self, api_key=None, api_secret=None):
+    def __init__(self, api_key=None, api_secret=None, headers=None):
+        """
+        params:
+            api_key (str): API key for authentication.
+            api_secret (str): API secret for authentication.
+            headers (dict): Additional headers for API requests.
+        """
         self.api_key = api_key
         self.api_secret = api_secret
-
         self.headers = {'X-MBX-APIKEY': self.api_key} if self.api_key else {}
-
-    def _query(self, url: str, params: dict, method: str = 'GET', headers=None):
         if headers:
             self.headers.update(headers)
-        return Request(headers=self.headers).send_request(method, url, params)
-
-    async def _async_query(self, url: str, params: dict, method: str = 'GET', headers=None):
-        if headers:
-            self.headers.update(headers)
-        return await AsyncRequest(headers=self.headers).send_request(method, url, params)
 
     @classmethod
-    async def _ws_query(cls, url: str, params: str, method: str = 'SUBSCRIBE', timeout_seconds=10):
+    def _query(cls,
+               url: str,
+               params: dict,
+               method: str = 'GET',
+               headers=None,
+               ):
+        """
+        params:
+            url (str): URL for the API endpoint.
+            params (dict): Query parameters for the API request.
+            method (str): HTTP method for the request (default is 'GET').
+            headers (dict): Additional headers for the request.
+
+        Returns:
+            dict: Response data from the API request.
+        """
+        return Request(headers=headers).send_request(method, url, params)
+
+    @classmethod
+    async def _async_query(cls,
+                           url: str,
+                           params: dict,
+                           method: str = 'GET',
+                           headers=None,
+                           ):
+        """
+        params:
+            url (str): URL for the API endpoint.
+            params (dict): Query parameters for the API request.
+            method (str): HTTP method for the request (default is 'GET').
+            headers (dict): Additional headers for the request.
+
+        Returns:
+            dict: Response data from the API request.
+        """
+        return await AsyncRequest(headers=headers).send_request(method, url, params)
+
+    @classmethod
+    async def _ws_query(cls,
+                        url: str,
+                        params: str,
+                        method: str = 'SUBSCRIBE',
+                        timeout_seconds=10,
+                        ):
+        """
+        params:
+            url (str): WebSocket URL for the API.
+            params (str): Parameters for the WebSocket request.
+            method (str): Method for the WebSocket request (default is 'SUBSCRIBE').
+            timeout_seconds (int): Timeout duration for the WebSocket connection.
+        """
         payload = {
             "method": method,
             "params": [params],
@@ -39,6 +86,13 @@ class API:
 
     @check_api_keys
     def get_payload(self, payload=None):
+        """
+          params:
+              payload (dict): Additional payload parameters.
+
+          Returns:
+              dict: Payload with timestamp and signature.
+          """
         if payload is None:
             payload = {}
         payload['timestamp'] = get_timestamp()
@@ -47,4 +101,11 @@ class API:
         return payload
 
     def _get_sign(self, payload: dict):
+        """
+        params:
+            payload (dict): Payload data for generating signature.
+
+        Returns:
+            str: HMAC signature.
+        """
         return hmac_hashing(self.api_secret, payload)
--- CryptoMathTrade/exchange/binance/core.py
+++ CryptoMathTrade/exchange/binance/core.py
@@ -3,43 +3,32 @@ from ..utils import _convert_kwargs_to_dict
 
 
 class Core:
-    # WS Market
-    @classmethod
+    def __init__(self,
+                 proxies=None,
+                 headers=None,
+                 cookies=None,
+                 auth=None,
+                 ):
+        self.proxies = proxies
+        self.headers = headers
+        self.cookies = cookies
+        self.auth = auth
+
+    def return_args(self, **kwargs):
+        if self.proxies:
+            kwargs['proxies'] = self.proxies
+        if self.headers:
+            kwargs['headers'] = self.headers
+        if self.cookies:
+            kwargs['cookies'] = self.cookies
+        if self.auth:
+            kwargs['auth'] = self.auth
+        return kwargs
+
+
+class MarketCore(Core):
     @_convert_kwargs_to_dict
-    def ws_get_depth_args(cls, params: dict) -> dict:
-        """Partial Book Depth Streams
-
-        Top bids and asks, Valid are 5, 10, or 20.
-
-        Stream Names: <symbol>@depth<levels> OR <symbol>@depth<levels>@100ms.
-
-        param:
-            symbol (str): the trading pair
-
-        Update Speed: 1000ms or 100ms
-        """
-        return {'method': 'SUBSCRIBE', 'url': URLS.WS_BASE_URL, 'params': f'{params["symbol"]}@depth'}
-
-    @classmethod
-    @_convert_kwargs_to_dict
-    def ws_get_trades_args(cls, params: dict) -> dict:
-        """Trade Streams
-
-         The Trade Streams push raw trade information; each trade has a unique buyer and seller.
-
-         Stream Name: <symbol>@trade
-
-         param:
-            symbol (str): the trading pair
-
-         Update Speed: Real-time
-         """
-        return {'method': 'SUBSCRIBE', 'url': URLS.WS_BASE_URL, 'params': f'{params["symbol"]}@trade'}
-
-    # Market
-    @classmethod
-    @_convert_kwargs_to_dict
-    def get_depth_args(cls, params: dict) -> dict:
+    def get_depth_args(self, params: dict) -> dict:
         """Get orderbook.
 
         GET /api/v3/depth
@@ -50,11 +39,10 @@ class Core:
             symbol (str): the trading pair
             limit (int, optional): limit the results. Default 100; max 5000. If limit > 5000, then the response will truncate to 5000.
         """
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.DEPTH_URL, 'params': params}
+        return self.return_args(method='GET', url=URLS.BASE_URL+URLS.DEPTH_URL, params=params)
 
-    @classmethod
     @_convert_kwargs_to_dict
-    def get_trades_args(cls, params: dict) -> dict:
+    def get_trades_args(self, params: dict) -> dict:
         """Recent Trades List
         Get recent trades (up to last 500).
 
@@ -66,11 +54,10 @@ class Core:
             symbol (str): the trading pair
             limit (int, optional): limit the results. Default 500; max 1000.
         """
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.TRADES_URL, 'params': params}
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TRADES_URL, params=params)
 
-    @classmethod
     @_convert_kwargs_to_dict
-    def get_ticker_args(cls, params: dict) -> dict:
+    def get_ticker_args(self, params: dict) -> dict:
         """24hr Ticker Price Change Statistics
 
         GET /api/v3/ticker/24hr
@@ -83,12 +70,12 @@ class Core:
         """
         if params.get('symbol') and params.get('symbols'):
             raise ValueError('symbol and symbols cannot be sent together.')  # custom error
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.TICKER_URL, 'params': params}
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TICKER_URL, params=params)
 
-    # Spot
-    @classmethod
+
+class SpotCore(Core):
     @_convert_kwargs_to_dict
-    def get_orders_args(cls, SpotObj, params: dict) -> dict:
+    def get_orders_args(self, SpotObj, params: dict) -> dict:
         """All Orders (USER_DATA)
 
         Get all account orders; active, canceled, or filled.
@@ -105,11 +92,10 @@ class Core:
             limit (int, optional): Default 500; max 1000.
             recvWindow (int, optional): The value cannot be greater than 60000
         """
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.GET_ORDERS_URL, 'params': SpotObj.get_payload(params)}
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.GET_ORDERS_URL, params=SpotObj.get_payload(params))
 
-    @classmethod
     @_convert_kwargs_to_dict
-    def get_open_order_args(cls, SpotObj, params: dict) -> dict:
+    def get_open_order_args(self, SpotObj, params: dict) -> dict:
         """Query Order (USER_DATA)
 
         Check an order's status.
@@ -126,11 +112,10 @@ class Core:
         """
         if not params.get('orderId') and not params.get('origClientOrderId'):
             raise ValueError('Param "origClientOrderId" or "orderId" must be sent, but both were empty/null!')   # custom error
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.ORDER_URL, 'params': SpotObj.get_payload(params)}
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.ORDER_URL, params=SpotObj.get_payload(params))
 
-    @classmethod
     @_convert_kwargs_to_dict
-    def get_open_orders_args(cls, SpotObj, params: dict) -> dict:
+    def get_open_orders_args(self, SpotObj, params: dict) -> dict:
         """Current Open Orders (USER_DATA)
 
         Get all open orders on a symbol.
@@ -143,11 +128,10 @@ class Core:
             symbol (str, optional)
             recvWindow (int, optional): The value cannot be greater than 60000
         """
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.OPEN_ORDERS_URL, params=SpotObj.get_payload(params))
 
-    @classmethod
     @_convert_kwargs_to_dict
-    def cancel_open_order_args(cls, SpotObj, params: dict) -> dict:
+    def cancel_open_order_args(self, SpotObj, params: dict) -> dict:
         """Cancel Order (TRADE)
 
         Cancel an active order.
@@ -163,11 +147,10 @@ class Core:
             newClientOrderId (str, optional)
             recvWindow (int, optional): The value cannot be greater than 60000
         """
-        return {'method': 'DELETE', 'url': URLS.BASE_URL + URLS.ORDER_URL, 'params': SpotObj.get_payload(params)}
+        return self.return_args(method='DELETE', url=URLS.BASE_URL + URLS.ORDER_URL, params=SpotObj.get_payload(params))
 
-    @classmethod
     @_convert_kwargs_to_dict
-    def cancel_open_orders_args(cls, SpotObj, params: dict) -> dict:
+    def cancel_open_orders_args(self, SpotObj, params: dict) -> dict:
         """Cancel Order (TRADE)
 
         Cancel an active order.
@@ -183,11 +166,10 @@ class Core:
             newClientOrderId (str, optional)
             recvWindow (int, optional): The value cannot be greater than 60000
         """
-        return {'method': 'DELETE', 'url': URLS.BASE_URL + URLS.OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
+        return self.return_args(method='DELETE', url=URLS.BASE_URL + URLS.OPEN_ORDERS_URL, params=SpotObj.get_payload(params))
 
-    @classmethod
     @_convert_kwargs_to_dict
-    def new_order_args(cls, SpotObj, params: dict) -> dict:
+    def new_order_args(self, SpotObj, params: dict) -> dict:
         """New Order (TRADE)
 
         Post a new order
@@ -213,10 +195,42 @@ class Core:
                     MARKET and LIMIT order types default to FULL, all other orders default to ACK.
             recvWindow (int, optional): The value cannot be greater than 60000
         """
-        return {'method': 'POST', 'url': URLS.BASE_URL + URLS.ORDER_URL, 'params': SpotObj.get_payload(params)}
+        return self.return_args(method='POST', url=URLS.BASE_URL + URLS.ORDER_URL, params=SpotObj.get_payload(params))
 
-    # Account
-    @classmethod
+
+class AccountCore(Core):
     @_convert_kwargs_to_dict
-    def get_deposit_address_args(cls, AccountObj, params: dict) -> dict:
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.GET_DEPOSIT_ADDRESS, 'params': AccountObj.get_payload(params)}
+    def get_deposit_address_args(self, AccountObj, params: dict) -> dict:
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.GET_DEPOSIT_ADDRESS, params=AccountObj.get_payload(params))
+
+
+class WSMarketCore(Core):
+    @_convert_kwargs_to_dict
+    def get_depth_args(self, params: dict) -> dict:
+        """Partial Book Depth Streams
+
+        Top bids and asks, Valid are 5, 10, or 20.
+
+        Stream Names: <symbol>@depth<levels> OR <symbol>@depth<levels>@100ms.
+
+        param:
+            symbol (str): the trading pair
+
+        Update Speed: 1000ms or 100ms
+        """
+        return self.return_args(method='SUBSCRIBE', url=URLS.WS_BASE_URL, params=f'{params["symbol"]}@depth')
+
+    @_convert_kwargs_to_dict
+    def get_trades_args(self, params: dict) -> dict:
+        """Trade Streams
+
+         The Trade Streams push raw trade information; each trade has a unique buyer and seller.
+
+         Stream Name: <symbol>@trade
+
+         param:
+            symbol (str): the trading pair
+
+         Update Speed: Real-time
+         """
+        return self.return_args(method='SUBSCRIBE', url=URLS.WS_BASE_URL, params=f'{params["symbol"]}@trade')
