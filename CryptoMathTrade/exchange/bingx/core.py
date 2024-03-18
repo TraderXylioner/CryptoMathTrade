b7bef57ef43c98918684839310c7eb1106d97005
T_X <treider.xylioner@gmail.com>
2024-03-18 20:48:41 +0200

    refactor core

--- CryptoMathTrade/exchange/bingx/core.py
+++ CryptoMathTrade/exchange/bingx/core.py
@@ -1,117 +1,117 @@
 from ._urls import URLS
+from .._core import Core
 from ..utils import _convert_kwargs_to_dict, get_timestamp
 
 
-# WS Market
-@_convert_kwargs_to_dict
-def ws_get_depth_args(params: dict) -> dict:
-    """Partial Book Depth Streams
+class MarketCore(Core):
+    @_convert_kwargs_to_dict
+    def get_depth_args(self, params: dict) -> dict:
+        """Get orderbook.
 
-    Top bids and asks.
+        GET /openApi/spot/v1/market/depth
 
-    Stream Names: <symbol>@depth<levels>.
+        https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#Query%20depth%20information
 
-    param:
-        symbol (str): the trading pair
-    """
-    return {'method': 'sub', 'url': URLS.WS_BASE_URL, 'params': f'{params["symbol"]}@depth'}
+        param:
+            symbol (str): the trading pair
 
+            limit (int, optional): limit the results. Default 100; max 1000. If limit > 1000, then the response will truncate to 1000
 
-@_convert_kwargs_to_dict
-def ws_get_trades_args(params: dict) -> dict:
-    """Trade Streams
+            recvWindow (int, optional).
+        """
+        params['limit'] = min(int(params.get('limit', 100)), 1000)
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.DEPTH_URL, params=params)
 
-     The Trade Streams push raw trade information; each trade has a unique buyer and seller.
+    @_convert_kwargs_to_dict
+    def get_trades_args(self, params: dict) -> dict:
+        """Recent Trades List
+        Get recent trades (up to last 100).
 
-     Stream Name: <symbol>@trade
+        GET /openApi/spot/v1/market/trades
 
-     param:
-        symbol (str): the trading pair
+        https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#Query%20transaction%20records
 
-     Update Speed: Real-time
-     """
-    return {'method': 'sub', 'url': URLS.WS_BASE_URL, 'params': f'{params["symbol"]}@trade'}
+        params:
+            symbol (str): the trading pair
 
+            limit (int, optional): limit the results. Default 100; max 100
 
-# Market
-@_convert_kwargs_to_dict
-def get_depth_args(params: dict) -> dict:
-    """Get orderbook.
+            recvWindow (int, optional).
+        """
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TRADES_URL, params=params)
 
-    GET /openApi/spot/v1/market/depth
+    @_convert_kwargs_to_dict
+    def get_ticker_args(self, params: dict) -> dict:
+        """24hr Ticker Price Change Statistics
 
-    https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#Query%20depth%20information
+        GET /openApi/spot/v1/ticker/24hr
 
-    param:
-        symbol (str): the trading pair
-        limit (int, optional): limit the results. Default 100; max 1000. If limit > 1000, then the response will truncate to 1000
-        recvWindow (int, optional).
-    """
-    params['limit'] = min(int(params.get('limit', 100)), 1000)
-    return {'method': 'GET', 'url': URLS.BASE_URL + URLS.DEPTH_URL, 'params': params}
+        https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#24-hour%20price%20changes
 
-
-@_convert_kwargs_to_dict
-def get_trades_args(params: dict) -> dict:
-    """Recent Trades List
-    Get recent trades (up to last 100).
-
-    GET /openApi/spot/v1/market/trades
-
-    https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#Query%20transaction%20records
-
-    params:
-        symbol (str): the trading pair
-        limit (int, optional): limit the results. Default 100; max 100
-        recvWindow (int, optional).
-    """
-    return {'method': 'GET', 'url': URLS.BASE_URL + URLS.TRADES_URL, 'params': params}
+        params:
+            symbol (str, optional): the trading pair.
+        """
+        params['timestamp'] = get_timestamp()
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TICKER_URL, params=params)
 
 
-@_convert_kwargs_to_dict
-def get_ticker_args(params: dict) -> dict:
-    """24hr Ticker Price Change Statistics
+class SpotCore(Core):
+    @_convert_kwargs_to_dict
+    def get_orders_args(self, SpotObj, params):
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.GET_ORDERS_URL, params=SpotObj.get_payload(params))
 
-    GET /openApi/spot/v1/ticker/24hr
+    # @convert_kwargs_to_dict
+    # def get_open_order_args(SpotObj, params):
+    #     if not params.get('orderId') and not params.get('origClientOrderId'):
+    #         raise ValueError('Param "origClientOrderId" or "orderId" must be sent, but both were empty/null!')
+    #     return {'method': 'GET', 'url': BASE_URL + ORDER_URL, 'params': SpotObj.get_payload(params)}
+    #
+    #
+    # @convert_kwargs_to_dict
+    # def get_open_orders_args(SpotObj, params):
+    #     return {'method': 'GET', 'url': BASE_URL + OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
+    #
+    #
+    # @convert_kwargs_to_dict
+    # def delete_open_orders_args(SpotObj, params):
+    #     return {'method': 'DELETE', 'url': BASE_URL + OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
+    #
+    #
+    # @convert_kwargs_to_dict
+    # def new_order_args(SpotObj, params):
+    #     return {'method': 'POST', 'url': BASE_URL + ORDER_URL, 'params': SpotObj.get_payload(params)}
+    #
+    #
+    # # Account
+    # @convert_kwargs_to_dict
+    # def get_deposit_address_args(AccountObj, params):
+    #     return {'method': 'GET', 'url': BASE_URL + GET_DEPOSIT_ADDRESS, 'params': AccountObj.get_payload(params)}
 
-    https://bingx-api.github.io/docs/#/en-us/spot/market-api.html#24-hour%20price%20changes
 
-    params:
-        symbol (str, optional): the trading pair.
-    """
-    params['timestamp'] = get_timestamp()
-    return {'method': 'GET', 'url': URLS.BASE_URL + URLS.TICKER_URL, 'params': params}
+class WSMarketCore(Core):
+    @_convert_kwargs_to_dict
+    def get_depth_args(self, params: dict) -> dict:
+        """Partial Book Depth Streams
 
+        Top bids and asks.
 
-# Spot
-@_convert_kwargs_to_dict
-def get_orders_args(SpotObj, params):
-    return {'method': 'GET', 'url': URLS.BASE_URL + URLS.GET_ORDERS_URL, 'params': SpotObj.get_payload(params)}
-#
-#
-# @convert_kwargs_to_dict
-# def get_open_order_args(SpotObj, params):
-#     if not params.get('orderId') and not params.get('origClientOrderId'):
-#         raise ValueError('Param "origClientOrderId" or "orderId" must be sent, but both were empty/null!')
-#     return {'method': 'GET', 'url': BASE_URL + ORDER_URL, 'params': SpotObj.get_payload(params)}
-#
-#
-# @convert_kwargs_to_dict
-# def get_open_orders_args(SpotObj, params):
-#     return {'method': 'GET', 'url': BASE_URL + OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
-#
-#
-# @convert_kwargs_to_dict
-# def delete_open_orders_args(SpotObj, params):
-#     return {'method': 'DELETE', 'url': BASE_URL + OPEN_ORDERS_URL, 'params': SpotObj.get_payload(params)}
-#
-#
-# @convert_kwargs_to_dict
-# def new_order_args(SpotObj, params):
-#     return {'method': 'POST', 'url': BASE_URL + ORDER_URL, 'params': SpotObj.get_payload(params)}
-#
-#
-# # Account
-# @convert_kwargs_to_dict
-# def get_deposit_address_args(AccountObj, params):
-#     return {'method': 'GET', 'url': BASE_URL + GET_DEPOSIT_ADDRESS, 'params': AccountObj.get_payload(params)}
+        Stream Names: <symbol>@depth<levels>.
+
+        param:
+            symbol (str): the trading pair
+        """
+        return self.return_args(method='sub', url=URLS.WS_BASE_URL, params=f'{params["symbol"]}@depth')
+
+    @_convert_kwargs_to_dict
+    def get_trades_args(self, params: dict) -> dict:
+        """Trade Streams
+
+         The Trade Streams push raw trade information; each trade has a unique buyer and seller.
+         Update Speed: Real-time
+
+         Stream Name: <symbol>@trade
+
+         param:
+            symbol (str): the trading pair
+         """
+        return self.return_args(method='sub', url=URLS.WS_BASE_URL, params=f'{params["symbol"]}@trade')
