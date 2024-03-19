05a303661e690e0bb3ce21f7cbad0ead89eb8cd7
T_X <treider.xylioner@gmail.com>
2024-03-19 18:32:18 +0200

    add URLS

--- CryptoMathTrade/exchange/bitget/_urls.py
+++ CryptoMathTrade/exchange/bitget/_urls.py
@@ -2,3 +2,5 @@ class URLS:
     # HTTP
     BASE_URL = 'https://api.bitget.com'
     DEPTH_URL = '/api/v2/spot/market/orderbook'
+    TRADES_URL = '/api/v2/spot/market/fills'
+    TICKER_URL = '/api/v2/spot/market/tickers'
