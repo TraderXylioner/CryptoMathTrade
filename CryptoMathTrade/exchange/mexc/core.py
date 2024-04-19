0a671b85a842d15b1b9542f9cb1c632fe3b21b48
TraderXylioner <treider.xylioner@gmail.com>
2024-04-19 17:54:19 +0300

    add MarketCore

--- CryptoMathTrade/exchange/mexc/core.py
+++ CryptoMathTrade/exchange/mexc/core.py
@@ -1,12 +1,11 @@
 from ._urls import URLS
+from .._core import Core
 from ..utils import _convert_kwargs_to_dict
 
 
-class Core:
-    # Market
-    @classmethod
+class MarketCore(Core):
     @_convert_kwargs_to_dict
-    def get_depth_args(cls, params: dict) -> dict:
+    def get_depth_args(self, params: dict) -> dict:
         """Get orderbook.
 
         GET /api/v3/depth
@@ -15,6 +14,36 @@ class Core:
 
         param:
             symbol (str): the trading pair
-            limit (int, optional): limit the results. Default 100; max 5000.
+
+            limit (int, optional): limit the results. Default 100; max 5000. If limit > 5000, then the response will truncate to 5000.
         """
-        return {'method': 'GET', 'url': URLS.BASE_URL + URLS.DEPTH_URL, 'params': params}
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.DEPTH_URL, params=params)
+
+    @_convert_kwargs_to_dict
+    def get_trades_args(self, params: dict) -> dict:
+        """Recent Trades List
+        Get recent trades (up to last 500).
+
+        GET /api/v3/trades
+
+        https://mexcdevelop.github.io/apidocs/spot_v3_en/#recent-trades-list
+
+        params:
+            symbol (str): the trading pair
+
+            limit (int, optional): limit the results. Default 500; max 1000.
+        """
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TRADES_URL, params=params)
+
+    @_convert_kwargs_to_dict
+    def get_ticker_args(self, params: dict) -> dict:
+        """24hr Ticker Price Change Statistics
+
+        GET /api/v3/ticker/24hr
+
+        https://mexcdevelop.github.io/apidocs/spot_v3_en/#24hr-ticker-price-change-statistics
+
+        params:
+            symbol (str, optional): the trading pair, if the symbol is not sent, tickers for all symbols will be returned in an array.
+        """
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TICKER_URL, params=params)
