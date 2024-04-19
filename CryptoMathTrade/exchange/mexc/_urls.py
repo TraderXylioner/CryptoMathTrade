36af0bd5ddf6fd682ded116ceb0241c8a4b185c4
TraderXylioner <treider.xylioner@gmail.com>
2024-04-19 17:54:25 +0300

    add URLS

--- CryptoMathTrade/exchange/mexc/_urls.py
+++ CryptoMathTrade/exchange/mexc/_urls.py
@@ -1,4 +1,8 @@
 class URLS:
     # HTTP
     BASE_URL = 'https://api.mexc.com'
-    DEPTH_URL = '/api/v3/depth'
+
+    # Market
+    DEPTH_URL = '/api/v3/depth'  # https://mexcdevelop.github.io/apidocs/spot_v3_en/#order-book
+    TRADES_URL = '/api/v3/trades'  # https://mexcdevelop.github.io/apidocs/spot_v3_en/#recent-trades-list
+    TICKER_URL = '/api/v3/ticker/24hr'  # https://mexcdevelop.github.io/apidocs/spot_v3_en/#24hr-ticker-price-change-statistics
