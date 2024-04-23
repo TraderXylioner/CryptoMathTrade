8ec0823ef13384ec32284753300dc3922843d857
TraderXylioner <treider.xylioner@gmail.com>
2024-04-23 18:47:03 +0300

    add URLS

--- CryptoMathTrade/exchange/htx/_urls.py
+++ CryptoMathTrade/exchange/htx/_urls.py
@@ -1,4 +1,9 @@
 class URLS:
     # HTTP
     BASE_URL = 'https://api.huobi.pro'
-    DEPTH_URL = '/market/depth'
+
+    # Market
+    DEPTH_URL = '/market/depth'  # https://huobiapi.github.io/docs/spot/v1/en/#get-market-depth
+    TRADES_URL = '/market/history/trade'  # https://huobiapi.github.io/docs/spot/v1/en/#get-the-most-recent-trades
+    TICKER_URL = '/market/detail/merged'  # https://huobiapi.github.io/docs/spot/v1/en/#get-latest-aggregated-ticker
+    TICKERS_URL = '/market/tickers'  # https://huobiapi.github.io/docs/spot/v1/en/#get-latest-tickers-for-all-pairs
