f22778e500b655477f15f2841254e2e99e92a791
TraderXylioner <treider.xylioner@gmail.com>
2024-04-23 16:50:30 +0300

    add URLS

--- CryptoMathTrade/exchange/okx/_urls.py
+++ CryptoMathTrade/exchange/okx/_urls.py
@@ -1,4 +1,9 @@
 class URLS:
     # HTTP
-    BASE_URL = 'https://www.okx.com/'
-    DEPTH_URL = '/api/v5/market/books'
+    BASE_URL = 'https://www.okx.com'
+
+    # Market
+    DEPTH_URL = '/api/v5/market/books'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-order-book
+    TRADES_URL = '/api/v5/market/trades'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-trades
+    TICKER_URL = '/api/v5/market/ticker'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-ticker
+    TICKERS_URL = '/api/v5/market/tickers'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-tickers
