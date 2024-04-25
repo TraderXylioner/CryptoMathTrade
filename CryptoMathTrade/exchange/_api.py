6664c55ba7fe3674206f2d9914b9e1607c5c3b77
TraderXylioner <treider.xylioner@gmail.com>
2024-04-25 18:32:27 +0300

    refactor api

---
+++ CryptoMathTrade/exchange/_api.py
@@ -0,0 +1,46 @@
+from ._request import Request, AsyncRequest
+
+
+class BaseAPI:
+    def __init__(self, headers=None):
+        self.headers = {}
+        if headers:
+            self.headers.update(headers)
+
+    @classmethod
+    def _query(cls,
+               url: str,
+               params: dict,
+               method: str = 'GET',
+               headers=None,
+               ):
+        """
+        params:
+            url (str): URL for the API endpoint.
+
+            params (dict): Query parameters for the API request.
+
+            method (str): HTTP method for the request (default is 'GET').
+
+            headers (dict): Additional headers for the request.
+        """
+        return Request(headers=headers).send_request(method, url, params)
+
+    @classmethod
+    async def _async_query(cls,
+                           url: str,
+                           params: dict,
+                           method: str = 'GET',
+                           headers=None,
+                           ):
+        """
+        params:
+            url (str): URL for the API endpoint.
+
+            params (dict): Query parameters for the API request.
+
+            method (str): HTTP method for the request (default is 'GET').
+
+            headers (dict): Additional headers for the request.
+        """
+        return await AsyncRequest(headers=headers).send_request(method, url, params)
--- CryptoMathTrade/exchange/bitmart/_api.py
+++ CryptoMathTrade/exchange/bitmart/_api.py
@@ -1,27 +1,5 @@
-from .._request import Request
+from .._api import BaseAPI
 
 
-class API:
-    def __init__(self, headers=None):
-        self.headers = {}
-        if headers:
-            self.headers.update(headers)
-
-    @classmethod
-    def _query(cls,
-               url: str,
-               params: dict,
-               method: str = 'GET',
-               headers=None,
-               ):
-        """
-        params:
-            url (str): URL for the API endpoint.
-
-            params (dict): Query parameters for the API request.
-
-            method (str): HTTP method for the request (default is 'GET').
-
-            headers (dict): Additional headers for the request.
-        """
-        return Request(headers=headers).send_request(method, url, params)
+class API(BaseAPI):
+    ...
