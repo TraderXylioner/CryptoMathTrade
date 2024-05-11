2b71469b5ccc97f4d5dafe37e79fdbfb40b6aeec
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 18:10:49 +0300

    add WebSocketMarket

--- CryptoMathTrade/exchange/htx/_market.py
+++ CryptoMathTrade/exchange/htx/_market.py
@@ -1,6 +1,10 @@
+import gzip
+import json
+from typing import Generator
+
 from ._api import API
-from ._serialization import _serialize_depth, _serialize_trades, _serialize_ticker
-from .core import MarketCore
+from ._serialization import _serialize_depth, _serialize_trades, _serialize_ticker, _serialize_trades_for_ws
+from .core import MarketCore, WSMarketCore
 from ..utils import validate_response
 from .._response import Response
 
@@ -127,3 +131,58 @@ class AsyncMarket(API):
             self._query(**MarketCore(headers=self.headers).get_ticker_args(symbol=symbol)))
         full_json_data = response.json()
         return _serialize_ticker(full_json_data, symbol, response)
+
+
+class WebSocketMarket(API):
+    async def get_depth(self,
+                        symbol: str,
+                        type: str | None = 'step0',
+                        ) -> Generator:
+        """Partial Book Depth Streams
+
+        Stream Names: market.{symbol}.depth.{type}
+
+        https://www.htx.com/en-us/opend/newApiPages/?id=7ec5342e-7773-11ed-9966-0242ac110003
+
+        param:
+            symbol (str): the trading pair
+
+            type (str, optional): Market depth aggregation level, details below	step0, step1, step2, step3, step4, step5. Default step0.
+
+        when type is set to "step0", the default value of "depth" is 150 instead of 20.
+
+        step0	No market depth aggregation
+        step1	Aggregation level = precision*10
+        step2	Aggregation level = precision*100
+        step3	Aggregation level = precision*1000
+        step4	Aggregation level = precision*10000
+        step5	Aggregation level = precision*100000
+
+        """
+
+        async for response in self._ws_query(
+            **WSMarketCore(headers=self.headers).get_depth_args(symbol=symbol, type=type)):
+            json_data = json.loads(gzip.decompress(response))
+            if 'tick' in json_data:
+                yield _serialize_depth(json_data['tick'], response)
+
+    async def get_trades(self,
+                         symbol: str,
+                         ) -> Generator:
+        """Trade Streams
+
+         The Trade Streams push raw trade information; each trade has a unique buyer and seller.
+         Update Speed: Real-time
+
+         Stream Name: market.{symbol}.trade.detail
+
+         https://www.htx.com/en-us/opend/newApiPages/?id=7ec53b69-7773-11ed-9966-0242ac110003
+
+         param:
+            symbol (str): the trading pair
+         """
+        async for response in self._ws_query(
+            **WSMarketCore(headers=self.headers).get_trades_args(symbol=symbol)):
+            json_data = json.loads(gzip.decompress(response))
+            if 'tick' in json_data:
+                yield _serialize_trades_for_ws(json_data['tick']['data'], response)
