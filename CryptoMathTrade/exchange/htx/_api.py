c48363390be943d38fcc3cea4a7a039ea0c332f6
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 18:10:41 +0300

    add _ws_query

--- CryptoMathTrade/exchange/htx/_api.py
+++ CryptoMathTrade/exchange/htx/_api.py
@@ -1,5 +1,43 @@
+import asyncio
+import gzip
+import json
+
 from .._api import BaseAPI
+from .._request import WebSocketRequest
 
 
 class API(BaseAPI):
-    ...
+    @classmethod
+    async def _ws_query(cls,
+                        url: str,
+                        params: str,
+                        method: str = 'SUBSCRIBE',
+                        timeout_seconds=10,
+                        headers=None,
+                        ):
+        """
+        params:
+            url (str): WebSocket URL for the API.
+
+            params (str): Parameters for the WebSocket request.
+
+            method (str): Method for the WebSocket request (default is 'SUBSCRIBE').
+
+            timeout_seconds (int): Timeout duration for the WebSocket connection.
+
+            headers (dict): Additional headers for the request.
+        """
+        payload = {
+            "method": method,
+            "sub": params,
+        }
+        connect = WebSocketRequest(headers=headers).open_connect(url=url, payload=payload)
+        async for client in connect:
+            while True:
+                data = await asyncio.wait_for(client.recv(), timeout=timeout_seconds)
+                if not data:
+                    raise ConnectionError  # custom error
+                json_data = json.loads(gzip.decompress(data))
+                if 'ping' in json_data:
+                    await client.send('pong')
+                yield data
