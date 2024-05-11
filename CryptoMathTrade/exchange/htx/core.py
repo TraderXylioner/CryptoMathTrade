0e9120ab509830a482e8c8de3800a3c6432526df
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 18:10:25 +0300

    add WSMarketCore

--- CryptoMathTrade/exchange/htx/core.py
+++ CryptoMathTrade/exchange/htx/core.py
@@ -66,3 +66,52 @@ class MarketCore(Core):
         """
         _url = URLS.TICKER_URL if 'symbol' in params else URLS.TICKERS_URL
         return self.return_args(method='GET', url=URLS.BASE_URL + _url, params=params)
+
+
+class WSMarketCore(Core):
+    @_convert_kwargs_to_dict
+    def get_depth_args(self, params: dict) -> dict:
+        """Partial Book Depth Streams
+
+        Stream Names: market.{symbol}.depth.{type}
+
+        https://www.htx.com/en-us/opend/newApiPages/?id=7ec5342e-7773-11ed-9966-0242ac110003
+
+        param:
+            symbol (str): the trading pair
+
+            type (str, optional): Market depth aggregation level, details below	step0, step1, step2, step3, step4, step5. Default step0.
+
+        when type is set to "step0", the default value of "depth" is 150 instead of 20.
+
+        step0	No market depth aggregation
+        step1	Aggregation level = precision*10
+        step2	Aggregation level = precision*100
+        step3	Aggregation level = precision*1000
+        step4	Aggregation level = precision*10000
+        step5	Aggregation level = precision*100000
+
+        """
+        return self.return_args(method='sub',
+                                url=URLS.WS_BASE_URL,
+                                params=f'market.{params["symbol"].lower()}.depth.{params["type"]}',
+                                )
+
+    @_convert_kwargs_to_dict
+    def get_trades_args(self, params: dict) -> dict:
+        """Trade Streams
+
+         The Trade Streams push raw trade information; each trade has a unique buyer and seller.
+         Update Speed: Real-time
+
+         Stream Name: market.{symbol}.trade.detail
+
+         https://www.htx.com/en-us/opend/newApiPages/?id=7ec53b69-7773-11ed-9966-0242ac110003
+
+         param:
+            symbol (str): the trading pair
+         """
+        return self.return_args(method='sub',
+                                url=URLS.WS_BASE_URL,
+                                params=f'market.{params["symbol"].lower()}.trade.detail',
+                                )
