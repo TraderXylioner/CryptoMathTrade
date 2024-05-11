69190552db4c6d6507b1ef60604d5b524eec9bdc
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 14:24:26 +0300

    move  api to Base

--- CryptoMathTrade/exchange/binance/_api.py
+++ CryptoMathTrade/exchange/binance/_api.py
@@ -1,11 +1,8 @@
-import asyncio
-import json
-
-from .._request import Request, AsyncRequest, WebSocketRequest
 from ..utils import get_timestamp, hmac_hashing, _prepare_params, check_api_keys
+from .._api import BaseAPI
 
 
-class API:
+class API(BaseAPI):
     def __init__(self, api_key=None, api_secret=None, headers=None):
         """
         params:
@@ -13,82 +10,12 @@ class API:
             api_secret (str): API secret for authentication.
             headers (dict): Additional headers for API requests.
         """
+        super().__init__()
         self.api_key = api_key
         self.api_secret = api_secret
         self.headers = {'X-MBX-APIKEY': self.api_key} if self.api_key else {}
         if headers:
             self.headers.update(headers)
-
-    @classmethod
-    def _query(cls,
-               url: str,
-               params: dict,
-               method: str = 'GET',
-               headers=None,
-               ):
-        """
-        params:
-            url (str): URL for the API endpoint.
-
-            params (dict): Query parameters for the API request.
-
-            method (str): HTTP method for the request (default is 'GET').
-
-            headers (dict): Additional headers for the request.
-        """
-        return Request(headers=headers).send_request(method, url, params)
-
-    @classmethod
-    async def _async_query(cls,
-                           url: str,
-                           params: dict,
-                           method: str = 'GET',
-                           headers=None,
-                           ):
-        """
-        params:
-            url (str): URL for the API endpoint.
-
-            params (dict): Query parameters for the API request.
-
-            method (str): HTTP method for the request (default is 'GET').
-
-            headers (dict): Additional headers for the request.
-        """
-        return await AsyncRequest(headers=headers).send_request(method, url, params)
-
-    @classmethod
-    async def _ws_query(cls,
-                        url: str,
-                        params: str,
-                        method: str = 'SUBSCRIBE',
-                        timeout_seconds=10,
-                        headers=None,
-                        ):
-        """
-        params:
-            url (str): WebSocket URL for the API.
-
-            params (str): Parameters for the WebSocket request.
-
-            method (str): Method for the WebSocket request (default is 'SUBSCRIBE').
-
-            timeout_seconds (int): Timeout duration for the WebSocket connection.
-
-            headers (dict): Additional headers for the request.
-        """
-        payload = {
-            "method": method,
-            "params": [params],
-        }
-        connect = WebSocketRequest(headers=headers).open_connect(url=url, payload=payload)
-        async for client in connect:
-            while True:
-                data = await asyncio.wait_for(client.recv(), timeout=timeout_seconds)
-                if not data:
-                    raise ConnectionError  # custom error
-                json_data = json.loads(data)
-                yield json_data
 
     @check_api_keys
     def get_payload(self, payload=None):
