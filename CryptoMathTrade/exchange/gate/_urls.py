48b4c6bf0d060f3dbdcb637025f8e0bc671d460c
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 19:19:25 +0300

    add WS_BASE_URL

--- CryptoMathTrade/exchange/gate/_urls.py
+++ CryptoMathTrade/exchange/gate/_urls.py
@@ -6,3 +6,6 @@ class URLS:
     DEPTH_URL = '/api/v4/spot/order_book'  # https://www.gate.io/docs/developers/apiv4/en/#retrieve-order-book
     TRADES_URL = '/api/v4/spot/trades'  # https://www.gate.io/docs/developers/apiv4/en/#retrieve-market-trades
     TICKER_URL = '/api/v4/spot/tickers'  # https://www.gate.io/docs/developers/apiv4/en/#retrieve-ticker-information
+
+    # WS
+    WS_BASE_URL = 'wss://api.gateio.ws/ws/v4/'
