a9641c180938ff1939f7aa3626a810e970a390e6
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 14:53:05 +0300

    add WS_BASE_URL

--- CryptoMathTrade/exchange/mexc/_urls.py
+++ CryptoMathTrade/exchange/mexc/_urls.py
@@ -6,3 +6,6 @@ class URLS:
     DEPTH_URL = '/api/v3/depth'  # https://mexcdevelop.github.io/apidocs/spot_v3_en/#order-book
     TRADES_URL = '/api/v3/trades'  # https://mexcdevelop.github.io/apidocs/spot_v3_en/#recent-trades-list
     TICKER_URL = '/api/v3/ticker/24hr'  # https://mexcdevelop.github.io/apidocs/spot_v3_en/#24hr-ticker-price-change-statistics
+
+    # WS
+    WS_BASE_URL = 'wss://wbs.mexc.com/ws'
