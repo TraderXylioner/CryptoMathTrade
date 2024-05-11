aec26cc9eb7835bcfbcb8581b00f4d315979aef9
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 18:10:30 +0300

    add WS_BASE_URL

--- CryptoMathTrade/exchange/htx/_urls.py
+++ CryptoMathTrade/exchange/htx/_urls.py
@@ -7,3 +7,6 @@ class URLS:
     TRADES_URL = '/market/history/trade'  # https://huobiapi.github.io/docs/spot/v1/en/#get-the-most-recent-trades
     TICKER_URL = '/market/detail/merged'  # https://huobiapi.github.io/docs/spot/v1/en/#get-latest-aggregated-ticker
     TICKERS_URL = '/market/tickers'  # https://huobiapi.github.io/docs/spot/v1/en/#get-latest-tickers-for-all-pairs
+
+    # WS
+    WS_BASE_URL = 'wss://api.huobi.pro/ws'
