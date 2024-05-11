f440acd42c17b54de71940fb14620072da119176
TraderXylioner <treider.xylioner@gmail.com>
2024-05-11 14:23:36 +0300

    add _ws_query

--- CryptoMathTrade/exchange/_api.py
+++ CryptoMathTrade/exchange/_api.py
@@ -1,4 +1,6 @@
-from ._request import Request, AsyncRequest
+import asyncio
+
+from ._request import Request, AsyncRequest, WebSocketRequest
 
 
 class BaseAPI:
@@ -44,3 +46,35 @@ class BaseAPI:
             headers (dict): Additional headers for the request.
         """
         return await AsyncRequest(headers=headers).send_request(method, url, params)
+
+    @classmethod
+    async def _ws_query(cls,
+                        url: str,
+                        params: str,
+                        method: str = 'SUBSCRIBE',
+                        timeout_seconds=10,
+                        headers=None,
+                        ):
+        """
+        params:
+            url (str): WebSocket URL for the API.
+
+            params (str): Parameters for the WebSocket request.
+
+            method (str): Method for the WebSocket request (default is 'SUBSCRIBE').
+
+            timeout_seconds (int): Timeout duration for the WebSocket connection.
+
+            headers (dict): Additional headers for the request.
+        """
+        payload = {
+            "method": method,
+            "params": [params],
+        }
+        connect = WebSocketRequest(headers=headers).open_connect(url=url, payload=payload)
+        async for client in connect:
+            while True:
+                data = await asyncio.wait_for(client.recv(), timeout=timeout_seconds)
+                if not data:
+                    raise ConnectionError  # custom error
+                yield data
