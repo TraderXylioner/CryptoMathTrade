3aa872cb3f6a4b954a3b05580ccd23c5234bd00e
TraderXylioner <treider.xylioner@gmail.com>
2024-05-13 12:52:23 +0300

    add WS_BASE_URL

--- CryptoMathTrade/exchange/bitget/_urls.py
+++ CryptoMathTrade/exchange/bitget/_urls.py
@@ -4,3 +4,6 @@ class URLS:
     DEPTH_URL = '/api/v2/spot/market/orderbook'
     TRADES_URL = '/api/v2/spot/market/fills'
     TICKER_URL = '/api/v2/spot/market/tickers'
+
+    # WS
+    WS_BASE_URL = 'wss://ws.bitget.com/v2/ws/public'
