ef4686529dfaa37ace64cf0078b72126504e4139
TraderXylioner <treider.xylioner@gmail.com>
2024-05-14 13:45:05 +0300

    debug

--- CryptoMathTrade/exchange/bingx/_serialization.py
+++ CryptoMathTrade/exchange/bingx/_serialization.py
@@ -3,7 +3,6 @@ from .._response import Response
 
 
 def _serialize_depth(data, response):
-    data = data['data']
     data['asks'] = data['asks'][::-1]
     return Response(data=OrderBook(asks=[Order(price=ask[0], volume=ask[1]) for ask in data['asks']],
                                    bids=[Order(price=bid[0], volume=bid[1]) for bid in data['bids']],
@@ -13,7 +12,6 @@ def _serialize_depth(data, response):
 
 
 def _serialize_trades(data, response):
-    data = data['data']
     return Response(data=[Trade(id=trade.get('id'),
                                 price=trade.get('price'),
                                 quantity=trade.get('qty'),
@@ -25,7 +23,6 @@ def _serialize_trades(data, response):
 
 
 def _serialize_trades_for_ws(data, response):
-    data = data['data']
     return Response(data=[Trade(id=data.get('t'),
                                 price=data.get('p'),
                                 quantity=data.get('q'),
@@ -37,7 +34,6 @@ def _serialize_trades_for_ws(data, response):
 
 
 def _serialize_ticker(data, response):
-    data = data['data']
     return Response(data=[Ticker(symbol=ticker.get('symbol'),
                                  priceChange=ticker.get('priceChange'),
                                  priceChangePercent=ticker.get('priceChangePercent')[:-1],
