c60629f0e32c6d78d7e01e51ebed5fef4935c7a9
TraderXylioner <treider.xylioner@gmail.com>
2024-05-14 19:24:35 +0300

    add Account, AsyncAccount

--- CryptoMathTrade/exchange/bingx/__init__.py
+++ CryptoMathTrade/exchange/bingx/__init__.py
@@ -1,4 +1,4 @@
 from ._market import Market, AsyncMarket, WebSocketMarket
 from ._spot import Spot
-from ._account import Account
+from ._account import Account, AsyncAccount
 from ._urls import URLS
