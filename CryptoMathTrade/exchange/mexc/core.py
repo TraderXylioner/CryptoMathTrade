5d8c21deae65ae714251340bf3adc6c1c22ac777
TraderXylioner <treider.xylioner@gmail.com>
2024-05-17 12:43:33 +0300

    add AccountCore

--- CryptoMathTrade/exchange/mexc/core.py
+++ CryptoMathTrade/exchange/mexc/core.py
@@ -88,3 +88,18 @@ class WSMarketCore(Core):
                                 url=URLS.WS_BASE_URL,
                                 params=f'spot@public.deals.v3.api@{params["symbol"].upper()}',
                                 )
+
+
+class AccountCore(Core):
+    @_convert_kwargs_to_dict
+    def get_balance_args(self, AccountObj, params):
+        """Query Assets
+
+        GET /api/v3/account
+
+        https://mexcdevelop.github.io/apidocs/spot_v3_en/#account-information
+        """
+        return self.return_args(method='GET',
+                                url=URLS.BASE_URL + URLS.GET_BALANCE,
+                                params=AccountObj.get_payload(params),
+                                )
