585bf10f1e911aafae319b48972afcdf38d7817d
TraderXylioner <treider.xylioner@gmail.com>
2024-05-17 13:48:23 +0300

    add __init__

--- CryptoMathTrade/exchange/bitmart/_api.py
+++ CryptoMathTrade/exchange/bitmart/_api.py
@@ -5,6 +5,22 @@ from .._request import WebSocketRequest
 
 
 class API(BaseAPI):
+    def __init__(self, api_key=None, api_secret=None, headers=None):
+        """
+        params:
+            api_key (str): API key for authentication.
+
+            api_secret (str): API secret for authentication.
+
+            headers (dict): Additional headers for API requests.
+        """
+        super().__init__()
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.headers = {'X-BM-KEY': self.api_key} if self.api_key else {}
+        if headers:
+            self.headers.update(headers)
+
     @classmethod
     async def _ws_query(cls,
                         url: str,
