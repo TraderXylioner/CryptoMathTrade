39a692b6c5c029cef8fa91a7797cdc9253b597c7
TraderXylioner <treider.xylioner@gmail.com>
2024-05-20 18:02:49 +0300

    add Account, AsyncAccount

---
+++ CryptoMathTrade/exchange/gate/_account.py
@@ -0,0 +1,42 @@
+from ._api import API
+from ._serialization import _serialize_balance
+from .core import AccountCore
+from .._response import Response
+from ..utils import validate_response
+
+
+class Account(API):
+    def get_balance(self, asset: str | None = None) -> Response:
+        """Query Assets
+
+        GET /unified/accounts
+
+        https://www.gate.io/docs/developers/apiv4/#get-unified-account-information
+
+        params:
+            asset (int, optional): If asset is blank, then query all positive assets user have.
+        """
+        response = validate_response(self._query(**AccountCore(headers=self.headers).get_balance_args(self,
+                                                                                                      asset=asset,
+                                                                                                      )))
+        json_data = response.json()
+        return _serialize_balance(json_data, response)
+
+
+class AsyncAccount(API):
+    async def get_balance(self, asset: str | None = None) -> Response:
+        """Query Assets
+
+        GET /unified/accounts
+
+        https://www.gate.io/docs/developers/apiv4/#get-unified-account-information
+
+        params:
+            asset (int, optional): If asset is blank, then query all positive assets user have.
+        """
+        response = validate_response(
+            await self._async_query(**AccountCore(headers=self.headers).get_balance_args(self,
+                                                                                         asset=asset,
+                                                                                         )))
+        json_data = response.json
+        return _serialize_balance(json_data, response)
