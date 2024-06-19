5b02164f035def72907faad4b6ae08947deec90f
TraderXylioner <treider.xylioner@gmail.com>
2024-06-19 17:21:44 +0300

    add TODO

--- CryptoMathTrade/exchange/bingx/__init__.py
+++ CryptoMathTrade/exchange/bingx/__init__.py
@@ -3,3 +3,5 @@ from ._account import Account, AsyncAccount
 from ._spot import Spot
 from ._urls import URLS
 from .core import MarketCore, WSMarketCore, AccountCore, SpotCore
+
+# TODO: _serialization and errors
