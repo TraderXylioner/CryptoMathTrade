22619c6ceaff3c7662c2ce65ae1af30883d7db3e
TraderXylioner <treider.xylioner@gmail.com>
2024-06-19 17:19:35 +0300

    refactor API

--- CryptoMathTrade/exchange/bingx/_api.py
+++ CryptoMathTrade/exchange/bingx/_api.py
@@ -22,14 +22,7 @@ class API(BaseAPI):
         if headers:
             self.headers.update(headers)
 
-    @classmethod
-    async def _ws_query(cls,
-                        url: str,
-                        params: str,
-                        method: str = 'sub',
-                        timeout_seconds=60,
-                        headers=None,
-                        ):
+    async def _ws_query(self, url: str, params: str, method: str = 'sub', timeout_seconds=60):
         """
         params:
             url (str): WebSocket URL for the API.
@@ -39,15 +32,13 @@ class API(BaseAPI):
             method (str): Method for the WebSocket request (default is 'SUBSCRIBE').
 
             timeout_seconds (int): Timeout duration for the WebSocket connection.
-
-            headers (dict): Additional headers for the request.
         """
         payload = {
             "reqType": method,
             "dataType": params,
             'id': str(uuid.uuid4())
         }
-        connect = WebSocketRequest(headers=headers).open_connect(url=url, payload=payload)
+        connect = WebSocketRequest(headers=self.headers).open_connect(url=url, payload=payload)
         async for client in connect:
             while True:
                 data = await asyncio.wait_for(client.recv(), timeout=timeout_seconds)
