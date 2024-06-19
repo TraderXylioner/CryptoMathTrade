c2460a925bb4927ec381b4c16448218fbe4ab76f
TraderXylioner <treider.xylioner@gmail.com>
2024-06-19 17:17:09 +0300

    refactor BaseAPI

--- CryptoMathTrade/exchange/_api.py
+++ CryptoMathTrade/exchange/_api.py
@@ -5,19 +5,21 @@ from .utils import check_api_keys, hmac_hashing, _prepare_params, get_timestamp
 
 
 class BaseAPI:
-    def __init__(self, api_key=None, api_secret=None, headers=None):
+    def __init__(self,
+                 api_key=None,
+                 api_secret=None,
+                 headers=None,
+                 ):
         self.api_key = api_key
         self.api_secret = api_secret
         self.headers = {}
         if headers:
             self.headers.update(headers)
 
-    @classmethod
-    def _query(cls,
+    def _query(self,
                url: str,
                params: dict = None,
                method: str = 'GET',
-               headers=None,
                ):
         """
         params:
@@ -26,17 +28,13 @@ class BaseAPI:
             params (dict): Query parameters for the API request.
 
             method (str): HTTP method for the request (default is 'GET').
-
-            headers (dict): Additional headers for the request.
         """
-        return Request(headers=headers).send_request(method, url, params)
+        return Request(headers=self.headers).send_request(method, url, params)
 
-    @classmethod
-    async def _async_query(cls,
+    async def _async_query(self,
                            url: str,
-                           params: dict,
+                           params: dict = None,
                            method: str = 'GET',
-                           headers=None,
                            ):
         """
         params:
@@ -45,18 +43,14 @@ class BaseAPI:
             params (dict): Query parameters for the API request.
 
             method (str): HTTP method for the request (default is 'GET').
-
-            headers (dict): Additional headers for the request.
         """
-        return await AsyncRequest(headers=headers).send_request(method, url, params)
+        return await AsyncRequest(headers=self.headers).send_request(method, url, params)
 
-    @classmethod
-    async def _ws_query(cls,
+    async def _ws_query(self,
                         url: str,
                         params: str,
                         method: str = 'SUBSCRIBE',
                         timeout_seconds=60,
-                        headers=None,
                         ):
         """
         params:
@@ -67,14 +61,12 @@ class BaseAPI:
             method (str): Method for the WebSocket request (default is 'SUBSCRIBE').
 
             timeout_seconds (int): Timeout duration for the WebSocket connection.
-
-            headers (dict): Additional headers for the request.
         """
         payload = {
             "method": method,
             "params": params,
         }
-        connect = WebSocketRequest(headers=headers).open_connect(url=url, payload=payload)
+        connect = WebSocketRequest(headers=self.headers).open_connect(url=url, payload=payload)
         async for client in connect:
             while True:
                 data = await asyncio.wait_for(client.recv(), timeout=timeout_seconds)
@@ -107,3 +99,8 @@ class BaseAPI:
             str: HMAC signature.
         """
         return hmac_hashing(self.api_secret, payload)
+
+    def return_args(self, **kwargs):
+        # if self.headers:
+        #     kwargs['headers'] = self.headers
+        return kwargs
