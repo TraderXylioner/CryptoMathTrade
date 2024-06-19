93a0dd1d4dd2755be5b1c3cef81aeade5f223389
TraderXylioner <treider.xylioner@gmail.com>
2024-06-19 17:19:08 +0300

    refactor SpotCore

--- CryptoMathTrade/exchange/bingx/core/_spot.py
+++ CryptoMathTrade/exchange/bingx/core/_spot.py
@@ -1,44 +1,147 @@
+from .._api import API
 from .._urls import URLS
-from ..._core import Core
-from ...utils import _convert_kwargs_to_dict
 
 
-class SpotCore(Core):
-    @_convert_kwargs_to_dict
-    def get_orders_args(self, SpotObj, params):
-        return self.return_args(method='GET',
-                                url=URLS.BASE_URL + URLS.GET_ORDERS_URL,
-                                params=SpotObj.get_payload(params),
-                                )
+class SpotCore(API):
+    def get_orders(self, **kwargs) -> dict:
+        """All Orders (USER_DATA)
 
-    @_convert_kwargs_to_dict
-    def get_open_order_args(self, SpotObj, params):
-        if not params.get('orderId') and not params.get('origClientOrderId'):
+        Get all account orders; active, canceled, or filled.
+
+        GET /openApi/spot/v1/trade/historyOrders
+
+        https://bingx-api.github.io/docs/#/en-us/spot/trade-api.html#Query%20Order%20History
+
+        params:
+            symbol (str)
+
+            orderId (int, optional)
+
+            startTime (int, optional): Unit: ms
+
+            endTime (int, optional): Unit: ms
+
+            pageIndex: (int, optional), Default: 1.
+
+            pageSize: (int, optional), Default: 100, Max 100.
+
+            status: (str, optional) FILLED / CANCELED / FAILED
+
+            type: (str, optional) MARKET / LIMIT / TAKE_STOP_LIMIT / TAKE_STOP_MARKET / TRIGGER_LIMIT / TRIGGER_MARKET
+        """
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.GET_ORDERS_URL, params=self.get_payload(kwargs))
+
+    def get_open_order(self, **kwargs) -> dict:
+        """Query Order (USER_DATA)
+
+        Check an order's status.
+
+        GET /openApi/spot/v1/trade/query
+
+        https://bingx-api.github.io/docs/#/en-us/spot/trade-api.html#Query%20Orders
+
+        params:
+            symbol (str)
+
+            orderId (int, optional)
+
+            clientOrderID (str, optional)
+        """
+        if not kwargs.get('orderId') and not kwargs.get('origClientOrderId'):
             raise ValueError('Param "origClientOrderId" or "orderId" must be sent, but both were empty/null!')
-        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.ORDER_URL, params=SpotObj.get_payload(params))
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.ORDER_URL, params=self.get_payload(kwargs))
 
-    @_convert_kwargs_to_dict
-    def get_open_orders_args(self, SpotObj, params):
-        return self.return_args(method='GET',
-                                url=URLS.BASE_URL + URLS.OPEN_ORDERS_URL,
-                                params=SpotObj.get_payload(params),
-                                )
+    def get_open_orders(self, **kwargs) -> dict:
+        """Current Open Orders (USER_DATA)
 
-    @_convert_kwargs_to_dict
-    def cancel_open_order_args(self, SpotObj, params: dict) -> dict:
+        Get all open orders on a symbol.
+
+        GET /api/v3/openOrders
+
+        https://bingx-api.github.io/docs/#/en-us/spot/trade-api.html#Query%20Open%20Orders
+
+        params:
+            symbol (str, optional)
+        """
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.OPEN_ORDERS_URL, params=self.get_payload(kwargs))
+
+    def cancel_open_order(self, **kwargs) -> dict:
+        """Cancel Order (TRADE)
+
+        Cancel an active order.
+
+        POST /openApi/spot/v1/trade/cancel
+
+        https://bingx-api.github.io/docs/#/en-us/spot/trade-api.html#Cancel%20an%20Order
+
+        params:
+            symbol (str)
+
+            orderId (int, optional)
+
+            clientOrderID (str, optional)
+        """
+        if not kwargs.get('orderId') and not kwargs.get('origClientOrderId'):
+            raise ValueError('Param "origClientOrderId" or "orderId" must be sent, but both were empty/null!')
         return self.return_args(method='POST',
                                 url=URLS.BASE_URL + URLS.CANCEL_ORDER_URL,
-                                params=SpotObj.get_payload(params),
+                                params=self.get_payload(kwargs),
                                 )
 
-    @_convert_kwargs_to_dict
-    def cancel_open_orders_args(self, SpotObj, params: dict) -> dict:
+    def cancel_open_orders(self, **kwargs) -> dict:
+        """Cancel Orders (TRADE)
+
+        Cancel an active orders.
+
+        POST /openApi/spot/v1/trade/cancelOpenOrders
+
+        https://bingx-api.github.io/docs/#/en-us/spot/trade-api.html#Cancel%20orders%20by%20symbol
+
+        params:
+            symbol (str, optional)
+        """
         return self.return_args(method='POST',
                                 url=URLS.BASE_URL + URLS.CANCEL_ORDERS_URL,
-                                params=SpotObj.get_payload(params),
+                                params=self.get_payload(kwargs),
                                 )
 
-    @_convert_kwargs_to_dict
-    def new_order_args(self, SpotObj, params: dict) -> dict:
-        return self.return_args(method='POST', url=URLS.BASE_URL + URLS.CREATE_ORDER_URL,
-                                params=SpotObj.get_payload(params))
+    def new_order(self, **kwargs) -> dict:
+        """New Market/Limit Order (TRADE)
+
+        Post a new order
+
+        POST /openApi/spot/v1/trade/order
+
+        https://bingx-api.github.io/docs/#/en-us/spot/trade-api.html#Create%20an%20Order
+
+        params:
+        for market order:
+
+            symbol (str)
+
+            side (Side)
+
+            quantity (float, optional)
+
+            quoteOrderQty (float, optional)
+
+        for limit order:
+
+            symbol (str)
+
+            side (Side)
+
+            price (float)
+
+            quantity (float, optional)
+
+            quoteOrderQty (float, optional)
+
+            timeInForce (str, optional). Default: GTC.
+        """
+        if not kwargs.get('quantity') and not kwargs.get('quoteOrderQty'):
+            raise ValueError('Param "quoteOrderQty" or "quantity" must be sent, but both were empty/null!')
+        return self.return_args(method='POST',
+                                url=URLS.BASE_URL + URLS.CREATE_ORDER_URL,
+                                params=self.get_payload(kwargs),
+                                )
