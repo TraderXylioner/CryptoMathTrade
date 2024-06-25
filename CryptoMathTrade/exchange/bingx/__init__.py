2fa4f2006d03144d1ff0324787ba33d34bcf98e6
TraderXylioner <treider.xylioner@gmail.com>
2024-06-25 17:10:01 +0300

    add WSAccountCore

--- CryptoMathTrade/exchange/bingx/__init__.py
+++ CryptoMathTrade/exchange/bingx/__init__.py
@@ -2,6 +2,6 @@ from ._market import Market, AsyncMarket, WebSocketMarket
 from ._account import Account, AsyncAccount
 from ._spot import Spot
 from ._urls import URLS
-from .core import MarketCore, WSMarketCore, AccountCore, SpotCore
+from .core import MarketCore, WSMarketCore, AccountCore, WSAccountCore, SpotCore
 
 # TODO: _serialization and errors
