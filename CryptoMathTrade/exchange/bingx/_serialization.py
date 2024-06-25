dc9d8f6a4822e51da3b3cb18dd2b5ee0fd30d00d
TraderXylioner <treider.xylioner@gmail.com>
2024-06-25 17:12:47 +0300

    debug serialization

--- CryptoMathTrade/exchange/bingx/_serialization.py
+++ CryptoMathTrade/exchange/bingx/_serialization.py
@@ -2,19 +2,25 @@ from ...types import OrderBook, Trade, Ticker, Order, Side, Balance, Kline, Full
 from .._response import Response
 
 
+def _serialize_listen_key(data, response):
+    return Response(data=data, response_object=response)
+
+
 def _serialize_depth(data, response):
+    if 'msg' in data:
+        raise Exception(data)
+
     data = data['data']
-    if not data:
-        raise Exception('error')
     data['asks'] = data['asks'][::-1]
     return Response(data=OrderBook(asks=[Order(price=ask[0], volume=ask[1]) for ask in data['asks']],
-                                   bids=[Order(price=bid[0], volume=bid[1]) for bid in data['bids']],
-                                   ),
-                    response_object=response,
-                    )
+                                   bids=[Order(price=bid[0], volume=bid[1]) for bid in data['bids']], ),
+                    response_object=response, )
 
 
 def _serialize_trades(data, response):
+    if 'msg' in data:
+        raise Exception(data)
+
     data = data['data']
     return Response(data=[Trade(id=trade.get('id'),
                                 price=trade.get('price'),
@@ -22,22 +28,13 @@ def _serialize_trades(data, response):
                                 side=Side.SELL if trade.get('buyerMaker') else Side.BUY,
                                 time=trade.get('time'),
                                 ) for trade in data],
-                    response_object=response,
-                    )
-
-
-def _serialize_trades_for_ws(data, response):
-    return Response(data=[Trade(id=data.get('t'),
-                                price=data.get('p'),
-                                quantity=data.get('q'),
-                                side=Side.SELL if data.get('m') else Side.BUY,
-                                time=data.get('T'),
-                                )],
-                    response_object=response,
-                    )
+                    response_object=response, )
 
 
 def _serialize_ticker(data, response):
+    if 'msg' in data:
+        raise Exception(data)
+
     data = data['data']
     return Response(data=[Ticker(symbol=ticker.get('symbol'),
                                  priceChange=ticker.get('priceChange'),
@@ -55,34 +52,10 @@ def _serialize_ticker(data, response):
                     )
 
 
-def _serialize_balance(data, response):
-    if 'data' not in data:
-        raise Exception(data)
-    return Response(data=[Balance(**i) for i in data['data']['balances']],
-                    response_object=response,
-                    )
-
-
-def _serialize_coins(data, response):
-    return Response(data=data, response_object=response)
-
-
-def _serialize_deposit_address(data, response):
-    return Response(data=data, response_object=response)
-
-
-def _serialize_withdraw(data, response):
-    return Response(data=data, response_object=response)
-
-
-def _serialize_symbols(data, response):
-    data = data['data']
-    return Response(data=data,
-                    response_object=response,
-                    )
-
-
 def _serialize_kline(data, response):
+    if 'msg' in data:
+        raise Exception(data)
+
     data = data['data']
     return Response(data=[Kline(openTime=i[0],
                                 openPrice=i[1],
@@ -94,6 +67,59 @@ def _serialize_kline(data, response):
                                 amount=i[7]
                                 ) for i in data],
                     response_object=response)
+
+
+def _serialize_balance(data, response):
+    if 'data' not in data:
+        raise Exception(data)
+
+    return Response(data=[Balance(**i) for i in data['data']['balances']],
+                    response_object=response,
+                    )
+
+
+def _serialize_trades_for_ws(data, response):
+    data = data['data']
+    return Response(data=[Trade(id=data.get('t'),
+                                price=data.get('p'),
+                                quantity=data.get('q'),
+                                side=Side.SELL if data.get('m') else Side.BUY,
+                                time=data.get('T'),
+                                )],
+                    response_object=response,
+                    )
+
+
+def _serialize_account_update_for_ws(data, response):
+    return Response(data=data, response_object=response)
+
+
+def _serialize_coins(data, response):
+    data = data['data']
+    return Response(data=data, response_object=response)
+
+
+def _serialize_deposit_address(data, response):
+    return Response(data=data, response_object=response)
+
+
+def _serialize_withdraw(data, response):
+    return Response(data=data, response_object=response)
+
+
+def _serialize_deposit_history(data, response):
+    return Response(data=data, response_object=response)
+
+
+def _serialize_withdraw_history(data, response):
+    return Response(data=data, response_object=response)
+
+
+def _serialize_symbols(data, response):
+    data = data['data']
+    return Response(data=data,
+                    response_object=response,
+                    )
 
 
 def _serialize_order(data, response):
