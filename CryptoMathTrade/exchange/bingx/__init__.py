271c66dcf56c86f168bc34c6943c7464cf8a9ef8
TraderXylioner <treider.xylioner@gmail.com>
2024-06-25 17:12:18 +0300

    import WebSocketAccount

--- CryptoMathTrade/exchange/bingx/__init__.py
+++ CryptoMathTrade/exchange/bingx/__init__.py
@@ -1,5 +1,5 @@
 from ._market import Market, AsyncMarket, WebSocketMarket
-from ._account import Account, AsyncAccount
+from ._account import Account, AsyncAccount, WebSocketAccount
 from ._spot import Spot
 from ._urls import URLS
 from .core import MarketCore, WSMarketCore, AccountCore, WSAccountCore, SpotCore
