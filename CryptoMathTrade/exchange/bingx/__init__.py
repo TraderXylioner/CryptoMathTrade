fe3fba59e13ea7e1adf4d6021eaa95756bbe17f8
TraderXylioner <treider.xylioner@gmail.com>
2024-07-11 13:39:34 +0300

    end deserialization

--- CryptoMathTrade/exchange/bingx/__init__.py
+++ CryptoMathTrade/exchange/bingx/__init__.py
@@ -3,5 +3,3 @@ from ._account import Account, AsyncAccount, WebSocketAccount
 from ._spot import Spot, AsyncSpot
 from ._urls import URLS
 from .core import MarketCore, WSMarketCore, AccountCore, WSAccountCore, SpotCore
-
-# TODO: _serialization and errors
