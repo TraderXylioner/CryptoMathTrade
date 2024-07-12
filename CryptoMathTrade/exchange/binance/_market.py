52f03e5516359a9b09b79bfb56ca4ab435c34241
TraderXylioner <treider.xylioner@gmail.com>
2024-07-12 12:21:03 +0300

    end market

--- CryptoMathTrade/exchange/binance/_market.py
+++ CryptoMathTrade/exchange/binance/_market.py
@@ -1,15 +1,16 @@
 import json
-from typing import Generator
 
 from ._api import API
-from ._serialization import _serialize_depth, _serialize_trades, _serialize_ticker, _serialize_trades_for_ws
+from ._deserialization import _deserialize_depth, _deserialize_trades, _deserialize_ticker, _deserialize_trades_for_ws, \
+    _deserialize_symbols, _deserialize_kline
 from .core import MarketCore, WSMarketCore
 from ..utils import validate_response
 from .._response import Response
+from ...types import OrderBook, Trade, Ticker, Symbol, Kline
 
 
 class Market(API):
-    def get_depth(self, symbol: str, limit: int = 100) -> Response:
+    def get_depth(self, symbol: str, limit: int = 100) -> Response[OrderBook, object]:
         """Get orderbook.
 
         GET /api/v3/depth
@@ -17,16 +18,15 @@ class Market(API):
         https://binance-docs.github.io/apidocs/spot/en/#order-book
 
         params:
-            symbol (str): the trading pair
+            symbol (str): the trading pair.
 
-            limit (int, optional): limit the results. Default 100; max 5000.
+            limit (int, optional): Default 100; max 5000.
         """
-        response = validate_response(
-            self._query(**MarketCore(headers=self.headers).get_depth_args(symbol=symbol, limit=limit)))
+        response = validate_response(self._query(**MarketCore.get_depth(self, symbol=symbol, limit=limit)))
         json_data = response.json()
-        return _serialize_depth(json_data, response)
+        return _deserialize_depth(json_data, response)
 
-    def get_trades(self, symbol: str, limit: int = 500) -> Response:
+    def get_trades(self, symbol: str, limit: int = 500) -> Response[list[Trade], object]:
         """Recent Trades List
 
         GET /api/v3/trades
@@ -38,12 +38,11 @@ class Market(API):
 
             limit (int, optional): limit the results. Default 500; max 1000.
         """
-        response = validate_response(
-            self._query(**MarketCore(headers=self.headers).get_trades_args(symbol=symbol, limit=limit)))
+        response = validate_response(self._query(**MarketCore.get_trades(self, symbol=symbol, limit=limit)))
         json_data = response.json()
-        return _serialize_trades(json_data, response)
+        return _deserialize_trades(json_data, response)
 
-    def get_ticker(self, symbol: str | None = None, symbols: list | None = None) -> Response:
+    def get_ticker(self, symbol: str = None, symbols: list = None) -> Response[list[Ticker], object]:
         """24hr Ticker Price Change Statistics
 
         GET /api/v3/ticker/24hr
@@ -57,14 +56,69 @@ class Market(API):
 
             symbols (list, optional): list of trading pairs
         """
-        response = validate_response(
-            self._query(**MarketCore(headers=self.headers).get_ticker_args(symbol=symbol, symbols=symbols)))
+        response = validate_response(self._query(**MarketCore.get_ticker(self, symbol=symbol, symbols=symbols)))
         json_data = response.json()
-        return _serialize_ticker(json_data, response)
+        return _deserialize_ticker(json_data, response)
+
+    def get_symbols(self, symbol: str = None, symbols: list = None) -> Response[list[Symbol], object]:
+        """Query Symbols
+
+        GET /api/v3/exchangeInfo
+
+        https://binance-docs.github.io/apidocs/spot/en/#exchange-information
+
+        params:
+            symbol (str, optional): the trading pair.
+
+            or / and
+
+            symbols (list, optional): list of trading pairs.
+        """
+        response = validate_response(self._query(**MarketCore.get_symbols(self, symbol=symbol, symbols=symbols)))
+        json_data = response.json()
+        return _deserialize_symbols(json_data, response)
+
+    def get_kline(self,
+                  symbol: str,
+                  interval: str,
+                  limit: int = 500,
+                  startTime: int = None,
+                  endTime: int = None,
+                  timeZone: str = None,
+                  ) -> Response[list[Kline], object]:
+        """Historical K-line data
+
+        GET /api/v3/klines
+
+        https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-data
+
+        params:
+            symbol (str): the trading pair.
+
+            interval (str): Time interval (1s, 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M).
+
+            limit (int, optional): Default 500; max 1000.
+
+            startTime (int, optional): Unit: ms.
+
+            endTime (int, optional): Unit: ms.
+
+            timeZone (str, optional): Default: 0 (UTC).
+        """
+        response = validate_response(self._query(**MarketCore.get_kline(self,
+                                                                        symbol=symbol,
+                                                                        interval=interval,
+                                                                        limit=limit,
+                                                                        startTime=startTime,
+                                                                        endTime=endTime,
+                                                                        timeZone=timeZone,
+                                                                        )))
+        json_data = response.json()
+        return _deserialize_kline(json_data, response)
 
 
 class AsyncMarket(API):
-    async def get_depth(self, symbol: str, limit: int = 100) -> Response:
+    async def get_depth(self, symbol: str, limit: int = 100) -> Response[OrderBook, object]:
         """Get orderbook.
 
         GET /api/v3/depth
@@ -72,16 +126,15 @@ class AsyncMarket(API):
         https://binance-docs.github.io/apidocs/spot/en/#order-book
 
         params:
-            symbol (str): the trading pair
+            symbol (str): the trading pair.
 
-            limit (int, optional): limit the results. Default 100; max 5000.
+            limit (int, optional): Default 100; max 5000.
         """
-        response = validate_response(
-            await self._async_query(**MarketCore(headers=self.headers).get_depth_args(symbol=symbol, limit=limit)))
+        response = validate_response(await self._async_query(**MarketCore.get_depth(self, symbol=symbol, limit=limit)))
         json_data = response.json
-        return _serialize_depth(json_data, response)
+        return _deserialize_depth(json_data, response)
 
-    async def get_trades(self, symbol: str, limit: int = 500) -> Response:
+    async def get_trades(self, symbol: str, limit: int = 500) -> Response[list[Trade], object]:
         """Recent Trades List
 
         GET /api/v3/trades
@@ -89,16 +142,15 @@ class AsyncMarket(API):
         https://binance-docs.github.io/apidocs/spot/en/#recent-trades-list
 
         params:
-            symbol (str): the trading pair
+            symbol (str): the trading pair.
 
-            limit (int, optional): limit the results. Default 500; max 1000.
+            limit (int, optional): Default 500; max 1000.
         """
-        response = validate_response(
-            await self._async_query(**MarketCore(headers=self.headers).get_trades_args(symbol=symbol, limit=limit)))
+        response = validate_response(await self._async_query(**MarketCore.get_trades(self, symbol=symbol, limit=limit)))
         json_data = response.json
-        return _serialize_trades(json_data, response)
+        return _deserialize_trades(json_data, response)
 
-    async def get_ticker(self, symbol: str | None = None, symbols: list | None = None) -> Response:
+    async def get_ticker(self, symbol: str = None, symbols: list = None) -> Response[list[Ticker], object]:
         """24hr Ticker Price Change Statistics
 
         GET /api/v3/ticker/24hr
@@ -106,24 +158,77 @@ class AsyncMarket(API):
         https://binance-docs.github.io/apidocs/spot/en/#24hr-ticker-price-change-statistics
 
         params:
-            symbol (str, optional): the trading pair
+            symbol (str, optional): the trading pair.
 
             or / and
 
-            symbols (list, optional): list of trading pairs
+            symbols (list, optional): list of trading pairs.
         """
         response = validate_response(
-            await self._async_query(**MarketCore(headers=self.headers).get_ticker_args(symbol=symbol, symbols=symbols)))
+            await self._async_query(**MarketCore.get_ticker(self, symbol=symbol, symbols=symbols)))
         json_data = response.json
-        return _serialize_ticker(json_data, response)
+        return _deserialize_ticker(json_data, response)
+
+    async def get_symbols(self, symbol: str = None, symbols: list = None) -> Response[list[Symbol], object]:
+        """Query Symbols
+
+        GET /api/v3/exchangeInfo
+
+        https://binance-docs.github.io/apidocs/spot/en/#exchange-information
+
+        params:
+            symbol (str, optional): the trading pair.
+
+            or / and
+
+            symbols (list, optional): list of trading pairs.
+        """
+        response = validate_response(
+            await self._async_query(**MarketCore.get_symbols(self, symbol=symbol, symbols=symbols)))
+        json_data = response.json
+        return _deserialize_symbols(json_data, response)
+
+    async def get_kline(self,
+                        symbol: str,
+                        interval: str,
+                        limit: int = 500,
+                        startTime: int = None,
+                        endTime: int = None,
+                        timeZone: str = None,
+                        ) -> Response[list[Kline], object]:
+        """Historical K-line data
+
+        GET /api/v3/klines
+
+        https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-data
+
+        params:
+            symbol (str): the trading pair.
+
+            interval (str): Time interval (1s, 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M).
+
+            limit (int, optional): Default 500; max 1000.
+
+            startTime (int, optional): Unit: ms.
+
+            endTime (int, optional): Unit: ms.
+
+            timeZone (str, optional): Default: 0 (UTC).
+        """
+        response = validate_response(await self._async_query(**MarketCore.get_kline(self,
+                                                                                    symbol=symbol,
+                                                                                    interval=interval,
+                                                                                    limit=limit,
+                                                                                    startTime=startTime,
+                                                                                    endTime=endTime,
+                                                                                    timeZone=timeZone,
+                                                                                    )))
+        json_data = response.json
+        return _deserialize_kline(json_data, response)
 
 
 class WebSocketMarket(API):
-    async def get_depth(self,
-                        symbol: str,
-                        limit: int | None = 20,
-                        interval: int | None = 1000,
-                        ) -> Generator:
+    async def get_depth(self, symbol: str, limit: int = 20, interval: int = 1000) -> Response[OrderBook, object]:
         """Partial Book Depth Streams
 
         Stream Names: <symbol>@depth<levels> OR <symbol>@depth<levels>@100ms.
@@ -137,19 +242,16 @@ class WebSocketMarket(API):
 
             interval (int, optional): 1000ms or 100ms.
         """
-
-        async for response in self._ws_query(
-            **WSMarketCore(headers=self.headers).get_depth_args(symbol=symbol,
-                                                                limit=limit,
-                                                                interval=interval,
-                                                                )):
+        async for response in self._ws_query(**WSMarketCore.get_depth(self,
+                                                                      symbol=symbol,
+                                                                      limit=limit,
+                                                                      interval=interval,
+                                                                      )):
             json_data = json.loads(response)
             if 'result' not in json_data:
-                yield _serialize_depth(json_data, response)
+                yield _deserialize_depth(json_data, response)
 
-    async def get_trades(self,
-                         symbol: str,
-                         ) -> Generator:
+    async def get_trades(self, symbol: str) -> Response[list[Trade], object]:
         """Trade Streams
 
          The Trade Streams push raw trade information; each trade has a unique buyer and seller.
@@ -162,7 +264,7 @@ class WebSocketMarket(API):
          params:
             symbol (str): the trading pair.
          """
-        async for response in self._ws_query(**WSMarketCore(headers=self.headers).get_trades_args(symbol=symbol)):
+        async for response in self._ws_query(**WSMarketCore.get_trades(self, symbol=symbol)):
             json_data = json.loads(response)
             if 'result' not in json_data:
-                yield _serialize_trades_for_ws(json_data, response)
+                yield _deserialize_trades_for_ws(json_data, response)
