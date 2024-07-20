2b2687d1afa6521fecff1c8505ac37cc56bb481f
TraderXylioner <treider.xylioner@gmail.com>
2024-07-20 09:56:35 +0300

    add urls

--- CryptoMathTrade/exchange/kucoin/_urls.py
+++ CryptoMathTrade/exchange/kucoin/_urls.py
@@ -7,6 +7,8 @@ class URLS:
     TRADES_URL = '/api/v1/market/histories'  # https://www.kucoin.com/docs/rest/spot-trading/market-data/get-trade-histories
     TICKER_URL = '/api/v1/market/stats'  # https://www.kucoin.com/docs/rest/spot-trading/market-data/get-24hr-stats
     TICKERS_URL = '/api/v1/market/allTickers'  # https://www.kucoin.com/docs/rest/spot-trading/market-data/get-all-tickers
+    SYMBOLS_URL = '/api/v2/symbols'  # https://www.kucoin.com/docs/rest/spot-trading/market-data/get-symbols-list
+    KLINE_URL = '/api/v1/market/candles'  # https://www.kucoin.com/docs/rest/spot-trading/market-data/get-klines
 
     # Account
     GET_BALANCE = '/api/v1/accounts'
