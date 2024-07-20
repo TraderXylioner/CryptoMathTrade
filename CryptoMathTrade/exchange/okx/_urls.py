de75437640e594bfede843cd9ccf4ac4e8df814e
TraderXylioner <treider.xylioner@gmail.com>
2024-07-20 09:58:38 +0300

    add urls

--- CryptoMathTrade/exchange/okx/_urls.py
+++ CryptoMathTrade/exchange/okx/_urls.py
@@ -7,6 +7,11 @@ class URLS:
     TRADES_URL = '/api/v5/market/trades'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-trades
     TICKER_URL = '/api/v5/market/ticker'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-ticker
     TICKERS_URL = '/api/v5/market/tickers'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-tickers
+    SYMBOLS_URL = '/api/v5/public/instruments'  # https://www.okx.com/docs-v5/en/#public-data-rest-api-get-instruments
+    KLINE_URL = '/api/v5/market/history-candles'  # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-candlesticks-history
+
+    # Account
+    GET_BALANCE = '/api/v5/account/balance'
 
     # WS
     WS_BASE_URL = 'wss://wspap.okx.com:8443/ws/v5/public?brokerId=9999'
