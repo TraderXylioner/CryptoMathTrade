8126f387288e6c5194e978b27a6380faa7b7a2c1
TraderXylioner <treider.xylioner@gmail.com>
2024-07-20 09:52:25 +0300

    end MarketCore

--- CryptoMathTrade/exchange/gate/core/_market.py
+++ CryptoMathTrade/exchange/gate/core/_market.py
@@ -1,10 +1,11 @@
+from .._api import API
 from .._urls import URLS
-from ..._core import Core
 from ...utils import check_require_params, replace_param
 
 
-class MarketCore(Core):
-    def get_depth_args(self, **kwargs) -> dict:
+class MarketCore(API):
+    @check_require_params(('symbol',))
+    def get_depth(self, **kwargs) -> dict:
         """Get orderbook.
 
         GET /api/v4/spot/order_book
@@ -16,11 +17,11 @@ class MarketCore(Core):
 
             limit (int, optional): limit the results. Default 10; max 5000.
         """
-        check_require_params(kwargs, ('symbol',))
         replace_param(kwargs, 'symbol', 'currency_pair')
         return self.return_args(method='GET', url=URLS.BASE_URL + URLS.DEPTH_URL, params=kwargs)
 
-    def get_trades_args(self, **kwargs) -> dict:
+    @check_require_params(('symbol',))
+    def get_trades(self, **kwargs) -> dict:
         """Recent Trades List
 
         GET /api/v4/spot/trades
@@ -32,11 +33,10 @@ class MarketCore(Core):
 
             limit (int, optional): limit the results. Default 100; max 1000.
         """
-        check_require_params(kwargs, ('symbol',))
         replace_param(kwargs, 'symbol', 'currency_pair')
         return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TRADES_URL, params=kwargs)
 
-    def get_ticker_args(self, **kwargs) -> dict:
+    def get_ticker(self, **kwargs) -> dict:
         """24hr Ticker Price Change Statistics
 
         GET /api/v4/spot/tickers
@@ -50,51 +50,85 @@ class MarketCore(Core):
             replace_param(kwargs, 'symbol', 'currency_pair')
         return self.return_args(method='GET', url=URLS.BASE_URL + URLS.TICKER_URL, params=kwargs)
 
+    def get_symbols(self, **kwargs) -> dict:
+        """Query Symbols
 
-class WSMarketCore(Core):
-    def get_depth_args(self, **kwargs) -> dict:
-        """Partial Book Depth Streams
+        GET /api/v4/spot/currency_pairs
 
-        Stream Names: spot.order_book_update
+        https://www.gate.io/docs/developers/apiv4/en/#list-all-currency-pairs-supported
+        """
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.SYMBOLS_URL, params=kwargs)
 
-        https://www.gate.io/docs/developers/apiv4/ws/en/#changed-order-book-levels
+    @check_require_params(('symbol',))
+    def get_kline(self, **kwargs) -> dict:
+        """Historical K-line data
+
+        GET /api/v4/spot/candlesticks
+
+        https://www.gate.io/docs/developers/apiv4/en/#market-candlesticks
 
         params:
-
             symbol (str): the trading pair.
 
-            limit (int, optional): limit the results. Valid are 5, 10, 20, 50 or 100.
+            interval (str, optional): Time interval (10s, 1m, 5m, 15m, 30m, 1h, 4h, 8h, 1d, 7d, 30d).
 
-            interval (int, optional): 1000ms or 100ms.
+            limit (int, optional): Default 500; max 1000.
 
+            startTime (int, optional): Unit: ms.
+
+            endTime (int, optional): Unit: ms.
         """
-        check_require_params(kwargs, ('symbol',))
-        return self.return_args(method='subscribe',
-                                url=URLS.WS_BASE_URL,
-                                params={'channel': 'spot.order_book',
-                                        'payload': [kwargs.get('symbol'),
-                                                    str(kwargs.get('limit')),
-                                                    f'{kwargs.get("interval")}ms'],
-                                        },
-                                )
+        replace_param(kwargs, 'symbol', 'currency_pair')
+        replace_param(kwargs, 'startTime', 'from')
+        replace_param(kwargs, 'endTime', 'to')
+        return self.return_args(method='GET', url=URLS.BASE_URL + URLS.KLINE_URL, params=kwargs)
 
-    def get_trades_args(self, **kwargs) -> dict:
-        """Trade Streams
+# TODO: WSMarketCore
 
-         The Trade Streams push raw trade information; each trade has a unique buyer and seller.
-         Update Speed: Real-time
-
-         Stream Name: spot.trades
-
-         https://www.gate.io/docs/developers/apiv4/ws/en/#client-subscription-2
-
-         params:
-            symbol (str): the trading pair.
-         """
-        check_require_params(kwargs, ('symbol',))
-        return self.return_args(method='subscribe',
-                                url=URLS.WS_BASE_URL,
-                                params={'channel': 'spot.trades',
-                                        'payload': [kwargs.get('symbol')],
-                                        },
-                                )
+# class WSMarketCore(Core):
+#     def get_depth_args(self, **kwargs) -> dict:
+#         """Partial Book Depth Streams
+#
+#         Stream Names: spot.order_book_update
+#
+#         https://www.gate.io/docs/developers/apiv4/ws/en/#changed-order-book-levels
+#
+#         params:
+#
+#             symbol (str): the trading pair.
+#
+#             limit (int, optional): limit the results. Valid are 5, 10, 20, 50 or 100.
+#
+#             interval (int, optional): 1000ms or 100ms.
+#
+#         """
+#         check_require_params(kwargs, ('symbol',))
+#         return self.return_args(method='subscribe',
+#                                 url=URLS.WS_BASE_URL,
+#                                 params={'channel': 'spot.order_book',
+#                                         'payload': [kwargs.get('symbol'),
+#                                                     str(kwargs.get('limit')),
+#                                                     f'{kwargs.get("interval")}ms'],
+#                                         },
+#                                 )
+#
+#     def get_trades_args(self, **kwargs) -> dict:
+#         """Trade Streams
+#
+#          The Trade Streams push raw trade information; each trade has a unique buyer and seller.
+#          Update Speed: Real-time
+#
+#          Stream Name: spot.trades
+#
+#          https://www.gate.io/docs/developers/apiv4/ws/en/#client-subscription-2
+#
+#          params:
+#             symbol (str): the trading pair.
+#          """
+#         check_require_params(kwargs, ('symbol',))
+#         return self.return_args(method='subscribe',
+#                                 url=URLS.WS_BASE_URL,
+#                                 params={'channel': 'spot.trades',
+#                                         'payload': [kwargs.get('symbol')],
+#                                         },
+#                                 )
