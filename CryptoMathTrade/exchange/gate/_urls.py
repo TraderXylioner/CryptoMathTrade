8beb30443d8c4da51d8931ab918aa78873ff9b23
TraderXylioner <treider.xylioner@gmail.com>
2024-07-20 09:52:38 +0300

    add urls

--- CryptoMathTrade/exchange/gate/_urls.py
+++ CryptoMathTrade/exchange/gate/_urls.py
@@ -6,6 +6,8 @@ class URLS:
     DEPTH_URL = '/api/v4/spot/order_book'  # https://www.gate.io/docs/developers/apiv4/en/#retrieve-order-book
     TRADES_URL = '/api/v4/spot/trades'  # https://www.gate.io/docs/developers/apiv4/en/#retrieve-market-trades
     TICKER_URL = '/api/v4/spot/tickers'  # https://www.gate.io/docs/developers/apiv4/en/#retrieve-ticker-information
+    SYMBOLS_URL = '/api/v4/spot/currency_pairs'  # https://www.gate.io/docs/developers/apiv4/en/#list-all-currency-pairs-supported
+    KLINE_URL = '/api/v4/spot/candlesticks'  # https://www.gate.io/docs/developers/apiv4/en/#market-candlesticks
 
     # Account
     GET_BALANCE = '/api/v4/spot/accounts'
